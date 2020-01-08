1d93ff1a94b2a1286d469fe744d0d1c3e1207f31
Jonathan barbeau <jobarbo@me.com>
2020-01-08 18:08:15 -0500

    added more code thing

--- sketches/p5_perlin.js
+++ sketches/p5_perlin.js
@@ -1,56 +1,38 @@
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
-const preload = p5 => {
-  // You can use p5.loadImage() here, etc...
-};
 
 const settings = {
   // Pass the p5 instance, and preload function if necessary
   p5: true,
-  //dimensions: [ 800, 800 ],
-  dimensions: 'a4',
+  dimensions: [ 11, 17 ],
+  //dimensions: 'a4',
   pixelsPerInch: 300,
   units: 'in',
   // Turn on a render loop
   animate: true
 };
 
+const preload = () => {
+  // You can use p5.loadImage() here, etc...
+};
+
 canvasSketch(() => {
-  colorMode(HSB,360,100,100,100);
-  rectMode(CENTER);
-  background(255);
-
-  const rectWidth = width/36;
-  const spacing = width/24;
-  let saturation = 100;
-  let brightness = 10;
-
-  for( x = spacing; x < width; x += spacing){
-    let hue = random(0,360);
-    for(y=spacing;y<height;y+=spacing){
-      stroke(hue,saturation,brightness);
-      fill(hue,saturation,brightness);
-      ellipse(x,y,rectWidth,rectWidth);
-      hue+=10;
-      if(hue>=360 ){
-        hue = 0;
-      }
-    }
-  }
-
-  for(lineNum = 0;lineNum < 60;lineNum++){
-    let randomX = random(0,width);
-    let randomY = random(0,height);
-    let diffX = random(-randomX,randomX);
-    let diffY = random(-randomY,randomY);
-    strokeWeight(random(5,10));
-    strokeCap(round);
-    stroke(360)
-    line(randomX,randomY,randomX+diffX,randomY+diffY);
-  }
   // Return a renderer, which is like p5.js 'draw' function
-  return ({ time, width, height,dimensions }) => {
+  //background(0);
+  let elX = (width/2)/settings.pixelsPerInch;
+  let elY = (height/2)/settings.pixelsPerInch;
+  return ({ p5, time, width, height }) => {
     // Draw with p5.js things
+    background(0);
+
+    //elX = elX + random(-0.05,0.05);
+    //elY = elY + random(-0.05,0.05);
+
+    fill(255,255);
+    noStroke();
+
+    const anim = sin(time - PI / 2) * 0.5 + 0.5;
+    ellipse(elX, elY+anim, 0.1,0.1);
   };
 }, settings);
