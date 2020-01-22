e92277c7f63b53a7ee76880ce39b4049258387c9
Jonathan barbeau <jobarbo@me.com>
2020-01-22 12:40:52 -0500

    messed with noise and broke a Galaxy

--- sketches/p5_perlin.js
+++ sketches/p5_perlin.js
@@ -25,31 +25,48 @@ canvasSketch((context) => {
   colorMode(HSB, 360, 100, 100, 100);
   blendMode(ADD);
   background(10);
-  let spacing = width/5;
-  let margin = width/10;
+  let margin = width/5;
+  let spacing = width-margin;
   let xoff = 0;
-  let yoff = 1;
-  let hoff = 0;
-  let container = spacing/4;
+  let yoff = 0.1;
+  let hoff = 0.2;
+  let woff = 0.2;
+  let container = spacing/2;
 
+  for(let i=0; i<2000;i++){
+    let starAlpha = random(10,50);
+    stroke(48,56,83,starAlpha);
+    strokeWeight(random(5,10));
+    point(random(width),random(height))
+  }
 
-  for(let ix = margin; ix <= (width-margin); ix = ix + spacing){
+  for(let ix = margin/2; ix < (width-margin); ix = ix + spacing){
     //noFill();
     //stroke(0);
-    for(let iy = margin; iy <= (height-margin); iy = iy + spacing){
-      for(let s = 0; s < spacing; s++){
-        let x = map(noise(xoff),0,1,ix-container,ix+container);
-        let y = map(noise(yoff),0,1,iy-container,iy+container);
+    for(let iy = margin/2; iy < (height-margin); iy = iy + spacing){
+      //strokeWeight(5)
+      //stroke(0,100,100)
+      //noFill();
+      //rect(ix,iy,spacing,spacing)
+      cx = ix+(spacing/2);
+      cy = iy+(spacing/2);
+
+      for(let s = 0; s < 50000; s++){
+        let x = map(noise(xoff),0,1,cx-container,cx+container);
+        let y = map(noise(yoff),0,1,cy-container,cy+container);
+        //let elW = map(noise(woff),0,1,7,10);
+        let elW =  map(abs(x-width/2), 0, width/2, 0, 30)
         let hue = map(noise(hoff),0,1,0,360);
 
         noStroke();
         //stroke(0,0,0)
-        fill(48,56,83,10)
-        ellipse(x,y,7,7);
+        fill(48,56,83,20)
+        ellipse(x,y,elW,elW);
 
-        xoff += 0.2;
-        yoff += 0.001;
+        xoff += 0.1;
+        yoff += 0.1;
         hoff += 0.01;
+        woff += 0.01;
       }
     }
   }
