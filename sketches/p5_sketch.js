ab8987dda117ce16466acf9be47252d66ecf70c7
Jonathan barbeau <jobarbo@me.com>
2020-01-29 14:11:24 -0500

    fixed grid system

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -5,7 +5,7 @@ new p5();
 const settings = {
   // Pass the p5 instance, and preload function if necessary
   p5: true,
-  dimensions: [ 30*300, 20*300 ],
+  dimensions: [ 12*300, 12*300 ],
   units: 'px',
   //pixelsPerInch: 72,
 
@@ -22,46 +22,25 @@ canvasSketch((context) => {
   // Sketch setup
   // Like p5.js 'setup' function
 
-  blendMode(BLEND);
+  blendMode(OVERLAY);
   colorMode(HSB, 360, 100, 100, 100);
-  background(191, 31, 20);
+  background(0, 0, 96);
 
-  let margin = width/20;
+  let margin = width/100;
   let wSpacing = width/10-margin;
-  let hSpacing = height-margin;
+  let hSpacing = height/10-margin;
   let xoff = 0.6;
-  let yoff = 0.1;
+  let yoff = 0.001;
   let hoff = 0.1;
-  let woff = 0.3;
-  let wContainer = wSpacing/2;
-  let hContainer = hSpacing/2.5;
+  let woff = 0.03;
+  let wContainer = wSpacing/1.5;
+  let hContainer = hSpacing/1.5;
 
   //displayStars();
-
-  for(let ix = margin/2; ix < (width-margin); ix = ix + wSpacing){
-    for(let iy = margin/2; iy < (height-margin); iy = iy + hSpacing){
-      //debugGrid(ix,iy,wSpacing,hSpacing);
-      cx = ix+(wSpacing/2);
-      cy = iy+(hSpacing/2);
-
-      for(let s = 0; s < 50000; s++){
-        let x = map(noise(xoff),0,1,cx-wContainer,cx+wContainer);
-        let y = map(noise(yoff),0,1,cy-hContainer,cy+hContainer);
-        let hue = map(noise(hoff),0,1,0,100);
-        let elW =  map(abs(x-width/2),0,width/2,4, 0)
-        //let elW = map(noise(woff),0,1,0,10);
-
-        noStroke();
-        fill(45, 84, 81,100)
-        ellipse(x,y,elW,elW);
-
-        xoff += 0.0005;
-        yoff += 0.05;
-        hoff += 0.1;
-        woff += 0.01;
-      }
-    }
+  window.mouseClicked = () => {
+    //paint(margin,wSpacing,hSpacing,xoff,yoff,hoff,woff,wContainer,hContainer);
   }
+  paint(margin,wSpacing,hSpacing,xoff,yoff,hoff,woff,wContainer,hContainer);
 
   // Return a renderer, which is like p5.js 'draw' function
   return ({ p5, time, width, height }) => {
@@ -70,6 +49,36 @@ canvasSketch((context) => {
 
   };
 }, settings);
+
+function paint(margin,wSpacing,hSpacing,xoff,yoff,hoff,woff,wContainer,hContainer){
+  console.log(keyCode);
+  for(let ix = margin; ix < (width-margin); ix = ix + wSpacing){
+    for(let iy = margin; iy < (height-margin); iy = iy + hSpacing){
+      //debugGrid(ix,iy,wSpacing,hSpacing);
+      cx = ix+(wSpacing/2);
+      cy = iy+(hSpacing/2);
+
+      for(let s = 0; s < wSpacing; s++){
+        let x = map(noise(xoff),0,1,cx-wContainer,cx+wContainer);
+        let y = map(noise(yoff),0,1,cy-hContainer,cy+hContainer);
+        let hue = map(noise(hoff),0,1,0,100);
+        //let elW =  map(abs(x-width/2),0,width,30, 0)
+        let elW = map(noise(woff),0,1,0,30);
+        let elA = map(elW,0,15,0,50);
+
+        //noStroke();
+        stroke(191, 5, 95,0);
+        fill(209, 57, 38,elA);
+        ellipse(x,y,elW,elW);
+
+        xoff += 0.005;
+        yoff += 0.005;
+        hoff += 0.1;
+        woff += 0.0005;
+      }
+    }
+  }
+}
 
 function displayStars(){
   for(let i=0; i<2000;i++){
