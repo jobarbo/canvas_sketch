4cdf68dee065cf2128c4f40c52c3bf4e406890cf
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-03-03 13:21:55 -0500

    glyphs print

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -5,7 +5,7 @@ new p5();
 const settings = {
   // Pass the p5 instance, and preload function if necessary
   p5: true,
-  dimensions: [ 12*300, 12*300 ],
+  dimensions: [12 * 300, 12 * 300],
   units: 'px',
   //pixelsPerInch: 72,
 
@@ -22,61 +22,67 @@ canvasSketch((context) => {
   // Sketch setup
   // Like p5.js 'setup' function
 
-  blendMode(BLEND);
+  blendMode(MULTIPLY);
   colorMode(HSB, 360, 100, 100, 100);
-  background(11, 55, 45);
+  background(28, 53, 85);
 
   let margin = 300;
-  let wSpacing = width/12;
-  let hSpacing = height/12;
+  let wSpacing = width / 12;
+  let hSpacing = height / 12;
+  let gridXSpacing = width / 12;
+  let gridYSpacing = height;
   let xoff = 0.6;
   let yoff = 0.001;
   let woff = 0.3;
-  let wContainer = wSpacing/2;
-  let hContainer = hSpacing/2;
+  let wContainer = wSpacing / 2;
+  let hContainer = hSpacing / 2;
+  let gridWContainer = gridXSpacing / 8;
+  let gridHContainer = gridYSpacing;
 
   //displayStars();
-  window.mousePressed = () => {
+  woff = random(0, 1);
+  yoff = random(0, 1);
+  xoff = random(0, 1);
 
-  }
+  gridLine(margin, gridXSpacing, gridYSpacing, xoff, yoff, woff, gridWContainer, gridHContainer);
   //paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
 
   // Return a renderer, which is like p5.js 'draw' function
   return ({ p5, time, width, height }) => {
     // Draw with p5.js things
-    if (mouseIsPressed){
-      woff = random(0,1);
-      yoff = random(0,1);
-      xoff = random(0,1);
+    if (mouseIsPressed) {
+      woff = random(0, 1);
+      yoff = random(0, 1);
+      xoff = random(0, 1);
+
 
       paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
     }
   };
 }, settings);
 
-function paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer){
-  for(let iy = margin; iy < (height-margin); iy = iy + hSpacing){
+function gridLine(margin, gridXSpacing, gridYSpacing, xoff, yoff, woff, gridWContainer, gridHContainer) {
+  for (let iy = margin; iy < (height - margin); iy = iy + gridYSpacing) {
+    for (let ix = margin; ix <= (width - margin); ix = ix + gridXSpacing) {
 
-    for(let ix = margin; ix < (width-margin); ix = ix + wSpacing){
       //debugGrid(ix,iy,wSpacing,hSpacing);
-      cx = ix+(wSpacing/2);
-      cy = iy+(hSpacing/2);
+      cx = ix;
 
-      for(let s = 0; s < wSpacing; s++){
+      for (let s = margin; s < gridYSpacing-margin; s++) {
 
-        let x = map(noise(xoff),0,1,cx-wContainer,cx+wContainer);
-        let y = map(noise(yoff),0,1,cy-hContainer,cy+hContainer);
-        let elW = map(noise(woff),0,1,0,10);
+        let x = map(noise(xoff), 0, 1, cx - gridWContainer, cx + gridWContainer);
+        let y = s;
+        let elW = map(noise(woff), 0, 1, 0, 10);
 
-        let elHue = map(elW,2,8,28,33,true);
-        let elSat = map(elW,2,8,40,60,true);
-        let elBright = map(elW,2,75,90,10,true);
-        let elAlpha = map(elW,4,8,0,60,true);
+        let elHue = map(elW, 2, 8, 18, 24, true);
+        let elSat = map(elW, 2, 8, 52, 62, true);
+        let elBright = map(elW, 2, 8, 70, 80, true);
+        let elAlpha = map(elW, 2, 8, 8, 20, true);
 
         noStroke();
         //stroke(190, 53, 89,0);
-        fill(elHue, elSat, elBright,elAlpha);
-        ellipse(x,y,elW,elW);
+        fill(elHue, elSat, elBright, elAlpha);
+        ellipse(x, y, elW, elW);
 
         xoff += 0.001;
         yoff += 0.001;
@@ -86,18 +92,44 @@ function paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer){
   }
 }
 
-function displayStars(){
-  for(let i=0; i<2000;i++){
-    let starAlpha = random(1,30);
-    stroke(48,56,83,starAlpha);
-    strokeWeight(random(5,10));
-    point(random(width),random(height))
+function paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer) {
+  for (let iy = margin; iy < (height - margin); iy = iy + hSpacing) {
+    for (let ix = margin; ix < (width - margin); ix = ix + wSpacing) {
+      //stroke(22, 58, 75,60)
+      //line(ix,margin,ix,height-margin);
+      //debugGrid(ix,iy,wSpacing,hSpacing);
+      cx = ix + (wSpacing / 2);
+      cy = iy + (hSpacing / 2);
+
+
+      for (let s = 0; s < wSpacing; s++) {
+
+        let x = map(noise(xoff), 0, 1, cx - wContainer, cx + wContainer);
+        let y = map(noise(yoff), 0, 1, cy - hContainer, cy + hContainer);
+        let elW = map(noise(woff), 0, 1, 6, 10);
+
+        let elHue = map(elW, 6, 9, 18, 24, true);
+        let elSat = map(elW, 6, 9, 52, 62, true);
+        let elBright = map(elW, 6, 9, 70, 80, true);
+        let elAlpha = map(elW, 6, 9, 5, 25, true);
+
+        noStroke();
+        //stroke(190, 53, 89,0);
+        fill(elHue, elSat, elBright, elAlpha);
+        ellipse(x, y, elW, elW);
+
+        xoff += 0.001;
+        yoff += 0.001;
+        woff += 0.1;
+      }
+    }
   }
 }
 
-function debugGrid(ix,iy,wSpacing,hSpacing){
+
+function debugGrid(ix, iy, wSpacing, hSpacing) {
   strokeWeight(5)
-  stroke(0,100,100)
+  stroke(0, 100, 100)
   noFill();
-  rect(ix,iy,wSpacing,hSpacing)
+  rect(ix, iy, wSpacing, hSpacing)
 }
