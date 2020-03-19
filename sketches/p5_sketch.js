ea2921d7697e2b6f0e0ea64fd5b4c73f394fd9ae
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-03-18 22:59:53 -0400

    painting effect

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -24,49 +24,42 @@ canvasSketch((context) => {
 
   blendMode(BLEND);
   colorMode(HSB, 360, 100, 100, 100);
-  background(45, 5, 98);
+  background(45, 5, 0);
 
   let margin = 0;
   let wSpacing = width;
   let hSpacing = height;
-  let xoff = 0.6;
-  let yoff = 0.001;
-  let woff = 0.3;
-  let initialX = width/2;
-  let initialY = height/2;
-  let wContainer = wSpacing / 1.5;
-  let hContainer = hSpacing / 1.5;
+  let xoff = 0.1;
+  let yoff = 0.4;
+  let woff = 0.03;
+  let minW = 2;
+  let maxW = 15;
+  let wContainer = wSpacing;
+  let hContainer = hSpacing;
 
   function paint() {
 
-    for (let s = 0; s < wSpacing; s++) {
+    let x = map(noise(xoff), 0, 1, margin, wContainer);
+    let y = map(noise(yoff), 0, 1, margin, hContainer);
+    let elW = map(noise(woff), 0, 1, minW, maxW);
 
-      let x = map(noise(xoff), 0, 1, wContainer, wContainer);
-      let y = map(noise(yoff), 0, 1, hContainer, hContainer);
-      let elW = map(noise(woff), 0, 1, 0, 10);
+    let elHue = map(elW, minW, maxW, 0, 45, true);
+    let elSat = map(elW, minW, maxW, 0, 5, true);
+    let elBright = map(elW, minW, maxW, 0, 100, true);
+    let elAlpha = map(elW, minW, maxW, 50, 100, true);
 
-      let elHue = map(elW, 2, 8, 45, 0, true);
-      let elSat = map(elW, 2, 8, 5, 75, true);
-      let elBright = map(elW, 2, 8, 98, 10, true);
-      let elAlpha = map(elW, 2, 8, 0, 100, true);
+    noStroke();
+    fill(elHue, elSat, elBright, elAlpha);
+    ellipse(x, y, elW, elW);
 
-      noStroke();
-      fill(elHue, elSat, elBright, elAlpha);
-      ellipse(x, y, elW, elW);
-
-      xoff += 0.1;
-      yoff += 0.1;
-      woff += 0.1;
-    }
+    xoff += 0.0008;
+    yoff += 0.0006;
+    woff += 0.005;
   }
 
   // Return a renderer, which is like p5.js 'draw' function
   return ({ p5, time, width, height }) => {
     // Draw with p5.js things
-    woff = random(0, 1);
-    yoff = random(0, 1);
-    xoff = random(0, 1);
-
     paint();
   };
 }, settings);
