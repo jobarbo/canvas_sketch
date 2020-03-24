3aa553de15f3d4767618050332287d42c9d4cb89
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-03-24 17:22:15 -0400

    seaweed print ready

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -24,20 +24,20 @@ canvasSketch((context) => {
 
   blendMode(BLEND);
   colorMode(HSB, 360, 100, 100, 100);
-  background(45, 5, 0);
+  background(137,20,99);
 
   let margin = 0;
   let wSpacing = width;
   let hSpacing = height;
-  let xoff = 0.1;
-  let yoff = 0.4;
-  let woff = 0.03;
-  let lenYOff = 0.2;
-  let lenXOff = 0.5;
-  let minLen = 25;
+  let xoff = 0.8;
+  let yoff = 0.01;
+  let woff = 0.3;
+  let lenYOff = 0.002;
+  let lenXOff = 0.2;
+  let minLen = 0;
   let maxLen = 200;
   let minW = 1;
-  let maxW = 2;
+  let maxW = 6;
   let wContainer = wSpacing;
   let hContainer = hSpacing;
 
@@ -49,26 +49,26 @@ canvasSketch((context) => {
     let lenY = map(noise(lenYOff), 0, 1, minLen, maxLen);
     let elW = map(noise(woff), 0, 1, minW, maxW);
 
-    let elHue = map(elW, minW, maxW, 0, 45, true);
-    let elSat = map(elW, minW, maxW, 0, 5, true);
-    let elBright = map(elW, minW, maxW, 0, 100, true);
-    let elAlpha = map(elW, minW, maxW, 50, 100, true);
+    let elHue = map(elW, minW, maxW, 69, 89, true);
+    let elSat = map(elW, minW, maxW, 49, 69, true);
+    let elBright = map(elW, minW, maxW, 71, 91, true);
+    let elAlpha = map(elW, minW, maxW, 0, 30, true);
 
     //noStroke();
     noFill();
-    //strokeWeight(elW)
+    strokeWeight(elW)
     stroke(elHue, elSat, elBright, elAlpha);
     line(x, y,x+lenX,y+lenY);
 
 
-    xoff += 0.001;
-    yoff += 0.001;
-    lenYOff += 0.1;
-    lenXOff += 0.001;
+    xoff += 0.0002;
+    yoff += 0.0001;
+    lenYOff += 0.004;
+    lenXOff += 0.003;
     woff += 0.005;
   }
 
-  for (let index = 0; index < 5000; index++) {
+  for (let index = 0; index < 40000; index++) {
 
     paint();
   }
