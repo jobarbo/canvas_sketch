e476ac0403c39318898e5b54ec6a27dc857f406a
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-04-14 19:14:35 -0400

    added mountains + sky effect

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -5,7 +5,7 @@ new p5();
 const settings = {
   // Pass the p5 instance, and preload function if necessary
   p5: true,
-  dimensions: [ 12*300, 12*300 ],
+  dimensions: [ 12*300, 18*300 ],
   units: 'px',
   //pixelsPerInch: 72,
 
@@ -23,12 +23,18 @@ canvasSketch((context) => {
   // Like p5.js 'setup' function
 
   let yoff = 0.0; // 2nd dimension of perlin noise
-  let minY = height/2;
-  let maxY = height/1.8;
-  let h = 209;
-  let s = 34;
-  let b = 77;
-  let vertexStep = 15;
+  let skyMinY = 0;
+  let skyMaxY = 0;
+  let landMinY = height/4;
+  let landMaxY = height/2;
+  let landHue = 209;
+  let landSat = 34;
+  let landBright = 77;
+  let skyHue = 17;
+  let skySat = 27;
+  let skyBright = 96;
+  let landscapeStep = 100;
+  let skyStep = 2;
 
   colorMode(HSB,360,100,100,100);
 
@@ -36,52 +42,75 @@ canvasSketch((context) => {
   return ({ p5, time, width, height }) => {
     // Draw with p5.js things
 
-    createLandscape(yoff,minY,maxY,h,s,b,vertexStep);
+    makeSky(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep);
     createSun();
+    createLandscape(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep);
+
 
   };
 }, settings);
 
-function createLandscape(yoff,minY,maxY,h,s,b,vertexStep) {
+function createLandscape(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep) {
 
-  background(202, 12, 92);
-  noStroke();
+  let xoff = 0;
 
-  // We are going to draw a polygon out of the wave points
-
-  let xoff = 0; // Option #1: 2D Noise
-
-  // Iterate over horizontal pixels
   for (let i = 0; i < 4; i++) {
     beginShape();
-    fill(h,s,b);
-    for (let x = 0; x <= width+vertexStep; x += vertexStep) {
+    fill(landHue,landSat,landBright);
+    for (let x = 0; x <= width+landscapeStep; x += landscapeStep) {
 
-      let y = map(noise(xoff, yoff), 0, 1, maxY, minY);
+      let y = map(noise(xoff, yoff), 0, 1, landMaxY, landMinY);
 
-      // Set the vertex
-      vertex(x, y);
+      curveVertex(x, y);
 
-      // Increment x dimension for noise
-      xoff += 0.008;
+      xoff += 0.1;
     }
-    minY += height/8;
-    maxY += height/8;
-    h += 5;
-    s += 5;
-    b -= 5
+    landMinY += height/8;
+    landMaxY += height/8;
+    landHue += 5;
+    landSat += 5;
+    landBright -= 5;
     vertex(width, height);
     vertex(0, height);
     endShape(CLOSE);
 
   }
 
-  // increment y dimension for noise
+  yoff += 0.1;
+
+}
+
+function makeSky(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep){
+  background(209,30,77);
+  noStroke();
+  // We are going to draw a polygon out of the wave points
+  let xoff = 0; // Option #1: 2D Noise
+  let skyAlpha = 5;
+  // Iterate over horizontal pixels
+  for (let i = 0; i < 4; i++) {
+    beginShape();
+    fill(skyHue,skySat,skyBright,skyAlpha);
+    for (let x = 0; x <= width+skyStep; x += skyStep) {
+      let y = map(noise(xoff, yoff), 0, 1, skyMaxY, skyMinY);
+      curveVertex(x, y);
+      xoff += 0.001;
+    }
+    skyMinY += height/20;
+    skyMaxY += height/12;
+    skyHue += 2;
+    skySat += 5;
+    skyBright += 1
+    skyAlpha += 5;
+    vertex(width, height);
+    vertex(0, height);
+    endShape(CLOSE);
+
+  }
   yoff += 0.1;
 
 }
 
 function createSun() {
-  fill( 39, 44, 95);
-  ellipse(550,550,width/8,width/8)
+  fill( 20, 50, 100);
+  ellipse(random(0,width),height/2.5,width/4,width/4)
 }
