737ec2ec18240ed33faeca477637db8bcbe660c8
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-04-15 11:32:57 -0400

    fixed function location

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -38,79 +38,83 @@ canvasSketch((context) => {
 
   colorMode(HSB,360,100,100,100);
 
+  function createLandscape() {
+
+    let xoff = 0;
+
+    for (let i = 0; i < 4; i++) {
+      beginShape();
+      fill(landHue,landSat,landBright);
+      for (let x = 0; x <= width+landscapeStep; x += landscapeStep) {
+
+        let y = map(noise(xoff, yoff), 0, 1, landMaxY, landMinY);
+
+        curveVertex(x, y);
+
+        xoff += 0.08;
+      }
+      landMinY += height/7;
+      landMaxY += height/6;
+      landHue += 5;
+      landSat += 5;
+      landBright -= 5;
+      vertex(width, height);
+      vertex(0, height);
+      endShape(CLOSE);
+
+    }
+
+    yoff += 0.1;
+
+  }
+
+  function makeSky(){
+    background(209,30,77);
+    noStroke();
+    // We are going to draw a polygon out of the wave points
+    let xoff = 0; // Option #1: 2D Noise
+    let skyAlpha = 5;
+    // Iterate over horizontal pixels
+    for (let i = 0; i < 4; i++) {
+      beginShape();
+      fill(skyHue,skySat,skyBright,skyAlpha);
+      for (let x = 0; x <= width+skyStep; x += skyStep) {
+        let y = map(noise(xoff, yoff), 0, 1, skyMaxY, skyMinY);
+        curveVertex(x, y);
+        xoff += 0.001;
+      }
+      skyMinY += height/15;
+      skyMaxY += height/8;
+      skyHue += 2;
+      skySat += 5;
+      skyBright += 1
+      skyAlpha += 5;
+      vertex(width, height);
+      vertex(0, height);
+      endShape(CLOSE);
+
+    }
+    yoff += 0.1;
+
+  }
+
+  function createSun() {
+    fill( 20, 50, 100);
+    ellipse(random(0,width),height/2.5,width/3,width/3)
+  }
+
+  makeSky();
+  createSun();
+  createLandscape();
+
   // Return a renderer, which is like p5.js 'draw' function
   return ({ p5, time, width, height }) => {
     // Draw with p5.js things
 
-    makeSky(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep);
-    createSun();
-    createLandscape(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep);
+
 
 
   };
 }, settings);
 
-function createLandscape(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep) {
 
-  let xoff = 0;
-
-  for (let i = 0; i < 4; i++) {
-    beginShape();
-    fill(landHue,landSat,landBright);
-    for (let x = 0; x <= width+landscapeStep; x += landscapeStep) {
-
-      let y = map(noise(xoff, yoff), 0, 1, landMaxY, landMinY);
-
-      curveVertex(x, y);
-
-      xoff += 0.1;
-    }
-    landMinY += height/8;
-    landMaxY += height/8;
-    landHue += 5;
-    landSat += 5;
-    landBright -= 5;
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-
-  }
-
-  yoff += 0.1;
-
-}
-
-function makeSky(yoff,skyMinY,skyMaxY,landMinY,landMaxY,skyHue,skySat,skyBright,landHue,landSat,landBright,landscapeStep,skyStep){
-  background(209,30,77);
-  noStroke();
-  // We are going to draw a polygon out of the wave points
-  let xoff = 0; // Option #1: 2D Noise
-  let skyAlpha = 5;
-  // Iterate over horizontal pixels
-  for (let i = 0; i < 4; i++) {
-    beginShape();
-    fill(skyHue,skySat,skyBright,skyAlpha);
-    for (let x = 0; x <= width+skyStep; x += skyStep) {
-      let y = map(noise(xoff, yoff), 0, 1, skyMaxY, skyMinY);
-      curveVertex(x, y);
-      xoff += 0.001;
-    }
-    skyMinY += height/20;
-    skyMaxY += height/12;
-    skyHue += 2;
-    skySat += 5;
-    skyBright += 1
-    skyAlpha += 5;
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-
-  }
-  yoff += 0.1;
-
-}
-
-function createSun() {
-  fill( 20, 50, 100);
-  ellipse(random(0,width),height/2.5,width/4,width/4)
-}
