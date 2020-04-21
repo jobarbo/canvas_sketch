abceabf6d8fab109849599d7cac37b989b7c0f0a
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-04-20 22:43:07 -0400

    creating 10 prints in js

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -5,12 +5,12 @@ new p5();
 const settings = {
   // Pass the p5 instance, and preload function if necessary
   p5: true,
-  dimensions: [ 12*300, 12*300 ],
+  dimensions: [ 12*300, 18*300 ],
   units: 'px',
   //pixelsPerInch: 72,
 
   // Turn on a render loop
-  animate: false
+  animate: true
 };
 
 const preload = () => {
@@ -24,10 +24,47 @@ canvasSketch((context) => {
 
   //blendMode(ADD);
   colorMode(HSB, 360, 100, 100, 100);
-  background(60,5,95);
+  //colorMode(RGB);
+  let x = 0;
+  let y = 0;
 
-  fill(0,0,0,100)
-  ellipse(width/2,height/2,30,30)
+  let h = 255;
+  let s = 0;
+  let b = 100;
+
+  let spacing = 50;
+
+  background(5, 30, 95);
+
+  function makeMaze() {
+
+    strokeCap(ROUND);
+    strokeWeight(16);
+    for(x = 0;x<width;x+=spacing){
+
+      for(y = 0;y<=height;y+=spacing){
+        let rand = random(0.5,1);
+        if (rand < 0.25) {
+          stroke(h,s,b);
+          line(x, y, x + spacing, y);
+        } else if (rand > 0.25 && rand < 0.5) {
+          stroke(h,s,b);
+          line(x, y+spacing , x, y);
+        } else if (rand > 0.5 && rand < 0.75) {
+          stroke(h,s,b);
+          line(x, y+spacing , x+spacing, y);
+        } else if (rand > 0.75 && rand < 1) {
+          stroke(h,s,b);
+          line(x, y , x+spacing, y+spacing);
+        }
+        if (y >= height){
+          console.log("done");
+        }
+      }
+    }
+  }
+
+  makeMaze();
 
   // Return a renderer, which is like p5.js 'draw' function
   return ({ p5, time, width, height }) => {
@@ -36,4 +73,3 @@ canvasSketch((context) => {
 
   };
 }, settings);
-
