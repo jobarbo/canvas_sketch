498ecd4a8e8baac89112ba0181b3a10387e2adab
Jonathan Barbeau <jonathan@instynctweb.com>
2020-06-01 09:34:33 -0400

    prettier

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -4,52 +4,45 @@ import Bubble from './bubble.js';
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
-const horizontal = 12*300;
-const vertical =  12*300;
+const horizontal = 12 * 300;
+const vertical = 12 * 300;
 
 const settings = {
-  // Pass the p5 instance, and preload function if necessary
-  p5: true,
-  dimensions: [ horizontal, vertical ],
-  units: 'px',
-  bleed: horizontal / 8,
-  //pixelsPerInch: 72,
+	// Pass the p5 instance, and preload function if necessary
+	p5: true,
+	dimensions: [horizontal, vertical],
+	units: 'px',
+	bleed: horizontal / 8,
+	//pixelsPerInch: 72,
 
-  // Turn on a render loop
-  animate: true
+	// Turn on a render loop
+	animate: true,
 };
 
 const preload = () => {
-  // You can use p5.loadImage() here, etc...
-
+	// You can use p5.loadImage() here, etc...
 };
 
 canvasSketch((context) => {
-  // Sketch setup
-  // Like p5.js 'setup' function
-  //const Bubble = require('./Bubble');
+	// Sketch setup
+	// Like p5.js 'setup' function
+	//const Bubble = require('./Bubble');
 
+	//blendMode(ADD);
+	colorMode(HSB, 360, 100, 100, 100);
+	background(60, 5, 95);
 
-  //blendMode(ADD);
-  colorMode(HSB, 360, 100, 100, 100);
-  background(60,5,95);
+	let bubble1 = new Bubble();
 
-  let bubble1 = new Bubble();
+	let bubble2 = new Bubble();
 
-  let bubble2 = new Bubble();
+	// Return a renderer, which is like p5.js 'draw' function
+	return ({ p5, time, width, height }) => {
+		// Draw with p5.js things
 
-  // Return a renderer, which is like p5.js 'draw' function
-  return ({ p5, time, width, height }) => {
-    // Draw with p5.js things
-
-    bubble1.move();
-    bubble2.move();
-    bubble1.display();
-    bubble2.display();
-
-  };
+		bubble1.move();
+		bubble2.move();
+		bubble1.display();
+		bubble2.display();
+	};
 }, settings);
-
-
-
-
