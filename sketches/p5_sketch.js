b36d51e00f6540fe4411c9c19608c2865af31a5c
Jonathan Barbeau <jonathan@instynctweb.com>
2020-06-18 12:26:49 -0400

    added bleed

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -5,15 +5,16 @@ const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
 const horizontal = 12 * 300;
-const vertical = 12 * 300;
+const vertical = 18 * 300;
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
 	p5: true,
 	dimensions: [horizontal, vertical],
 	units: 'px',
-	bleed: horizontal / 8,
-	//pixelsPerInch: 72,
+	bleed: 1 * 300,
+	// dimension 14 x 20 avec bleed
+	// pixelsPerInch: 72,
 
 	// Turn on a render loop
 	animate: true,
@@ -23,7 +24,7 @@ const preload = () => {
 	// You can use p5.loadImage() here, etc...
 };
 
-canvasSketch((context) => {
+canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup
 	// Like p5.js 'setup' function
 	//const Bubble = require('./Bubble');
@@ -31,18 +32,28 @@ canvasSketch((context) => {
 	//blendMode(ADD);
 	colorMode(HSB, 360, 100, 100, 100);
 	background(60, 5, 95);
+	let bubble = [];
 
-	let bubble1 = new Bubble();
-	let bubble3 = new Bubble();
-	let bubble2 = new Bubble();
+	for (let i = 0; i <= 50; i++) {
+		bubble[i] = new Bubble();
+	}
+
+	// Visualize the trim area with a yellow guide (ignored on export)
 
 	// Return a renderer, which is like p5.js 'draw' function
-	return ({ p5, time, width, height }) => {
+	return ({ p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
 		// Draw with p5.js things
 
-		bubble1.move();
-		bubble2.move();
-		bubble1.display();
-		bubble2.display();
+		for (let i = 0; i <= 50; i++) {
+			bubble[i].move();
+			bubble[i].display();
+		}
+
+		if (!exporting && bleed > 0) {
+			stroke(0);
+			noFill();
+			strokeWeight(10);
+			rect(bleed, bleed, trimWidth, trimHeight);
+		}
 	};
 }, settings);
