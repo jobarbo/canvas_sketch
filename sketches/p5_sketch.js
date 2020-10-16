14b6829320d34fcab5cc619a09eb8d6f03eae5c6
Jonathan Barbeau <jonathan@instynctweb.com>
2020-10-16 19:37:25 -0400

    inktober/jour16

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -1,5 +1,5 @@
 // Import sketch objects
-import Branch from './branch.js';
+//import Branch from './branch.js';
 
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
@@ -15,6 +15,7 @@ const settings = {
 	dimensions: [horizontal, vertical],
 	units: 'px',
 	bleed: 1 * 300,
+	//context: 'webgl',
 
 	// Turn on a render loop
 	animate: true,
@@ -27,36 +28,24 @@ const preload = () => {
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup
 	// Like p5.js 'setup' function
-	//const Bubble = require('./Bubble');
 
-	//blendMode(ADD);
+	let x = 0.01;
+	let y = 0;
+	let z = 0;
+
+	let a = 10;
+	let b = 25;
+	let c = 10.0 / 5.0;
+
+	let points = new Array();
+
+	blendMode(ADD);
 	colorMode(HSB, 360, 100, 100, 100);
-
-	let tree = [];
-	let wCenter = width / 2;
-	let baseHeight = height / 1.2;
-
-	let a = createVector(wCenter, baseHeight);
-	let b = createVector(wCenter, baseHeight - 2000);
-	let root = new Branch(a, b, 200);
 	background(0, 0, 10);
-	tree[0] = root;
-
-	function mousePressed() {
-		for (var i = tree.length - 1; i >= 0; i--) {
-			if (!tree[i].finished) {
-				tree.push(tree[i].branchA());
-				tree.push(tree[i].branchB());
-			}
-			tree[i].finished = true;
-		}
-	}
-
 	// Return a renderer, which is like p5.js 'draw' function
-	return ({ p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
+	return ({ time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
 		// Draw with p5.js things
 
-		//background(0, 0, 10);
 		// -- Frame -- //
 		strokeWeight(15);
 		stroke(60, 5, 95, 100);
@@ -64,18 +53,38 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		rect(600, 600, width - 1200, height - 1200);
 		// --      -- //
 
-		for (let i = 0; i < tree.length; i++) {
-			tree[i].show();
-			tree[i].jitter();
+		let dt = 0.01;
+		let dx = a * (y - x) * dt;
+		let dy = (x * (b - z) - y) * dt;
+		let dz = (x * y - c * z) * dt;
+		x = x + dx;
+		y = y + dy;
+		z = z + dz;
+
+		points.push(new p5.Vector(x, y, z));
+		translate(0, 0, -80);
+		//let camX = map(mouseX, 0, width, -5000, 5000);
+		//let camY = map(mouseY, 0, height, -5000, 5000);
+		//camera(camX, camY, height / 2.0 / tan((PI * 30.0) / 90.0), 0, 0, 0, 0, 1, 0);
+		translate(width / 2, height / 2);
+		strokeWeight(0.1);
+		scale(85);
+		//strokeCap(SQUARE);
+		//point(x, y);
+
+		//ellipse(x, y, 0.1, 0.1);
+		//line(prevX, prevY, x, y);
+		beginShape();
+
+		for (let v of points) {
+			stroke(60, 5, 95, 10);
+			noFill();
+			curveVertex(v.x, v.y, v.z);
+			//var offset = p5.Vector.random3D();
+			//offset.mult(0.1);
+			//v.add(offset);
 		}
-
-		root.show();
-		translate(wCenter, baseHeight);
-
-		if (mouseIsPressed) {
-			mousePressed();
-		}
-
+		endShape();
 		exporting = true;
 		if (!exporting && bleed > 0) {
 			stroke(0);
