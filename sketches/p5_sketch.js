dde7abbe2b2d73a00eaca6f9d0e2e554f2ee63b5
Jonathan Barbeau <jonathan@instynctweb.com>
2020-10-22 23:41:51 -0400

    inktober/jour22

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -24,23 +24,25 @@ canvasSketch((context) => {
 	blendMode(BLEND);
 	colorMode(HSB, 360, 100, 100, 100);
 	background(0, 0, 10);
+	ellipseMode(CENTER);
 
 	let margin = 1200;
 	let wSpacing = 500;
-	let hSpacing = 800;
+	let hSpacing = 500;
 	let xoff = 0.0006;
 	let yoff = 0.001;
 	let woff = 0.00003;
 	let wContainer = wSpacing / 5;
 	let hContainer = hSpacing;
-	let minW = 20;
-	let maxW = 30;
+	let minW = wSpacing / 2;
+	let maxW = wSpacing / 2;
 
 	strokeWeight(15);
 	stroke(60, 5, 95, 100);
 	noFill();
-	rect(600, 560, width - 1200, height - 1100);
+	rect(600, 600, width - 1200, height - 1200);
 
+	rectMode(CENTER);
 	paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, minW, maxW);
 
 	// Return a renderer, which is like p5.js 'draw' function
@@ -50,42 +52,31 @@ canvasSketch((context) => {
 }, settings);
 
 function paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, minW, maxW) {
-	for (let iy = 600; iy < height - 600; iy = iy + hSpacing) {
+	for (let iy = margin; iy < height - margin; iy = iy + hSpacing) {
 		for (let ix = margin; ix <= width - margin; ix = ix + wSpacing) {
-			//debugGrid(ix,iy,wSpacing,hSpacing);
-
+			//debugGrid(ix, iy, wSpacing, hSpacing);
 			cx = ix;
 			cy = iy;
 
-			let y = cy;
-			let xoffIteration = 0.0009;
+			cx2 = random(cx - maxW / 2, cx + maxW / 2);
+			cy2 = random(cy - maxW / 2, cy + maxW / 2);
+			elw = random(minW, maxW);
 
-			for (let s = 0; s < hSpacing; s++) {
-				let x = map(noise(xoff), 0, 1, cx - wContainer, cx + wContainer);
-				let elW = map(noise(woff), 0, 1, minW, maxW);
+			strokeWeight(20);
+			noFill();
+			stroke(60, 5, 95, 100);
+			ellipse(cx, cy, elw, elw);
 
-				let elHue = map(elW, minW, maxW, 60, 65, true);
-				let elSat = map(elW, minW, maxW, 0, 10, true);
-				let elBright = map(elW, minW, maxW, 84, 100, true);
-				let elAlpha = map(elW, minW, maxW, 50, 90, true);
+			noStroke();
+			fill(60, 5, 95, 100);
+			ellipse(cx2, cy2, elw, elw);
 
-				//noStroke();
-				strokeWeight(elW);
-				stroke(elHue, elSat, elBright, elAlpha);
-				fill(elHue, elSat, elBright, elAlpha);
-				line(x, y, x + elW, y + elW);
-
-				y++;
-
-				xoff += xoffIteration;
-				yoff += 0.1;
-				woff += 0.0005;
-				//xoffIteration = xoffIteration + 0.000001;
-			}
+			noFill();
+			stroke(0, 0, 10);
+			ellipse(cx2, cy2, elw, elw);
 		}
 	}
 }
-
 function debugGrid(ix, iy, wSpacing, hSpacing) {
 	strokeWeight(5);
 	stroke(0, 100, 100);
