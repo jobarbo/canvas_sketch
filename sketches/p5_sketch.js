ebdcae2e05dbb877ef353d470d0090ac0bc98c25
Jonathan Barbeau <jonathan@instynctweb.com>
2020-10-26 23:28:31 -0400

    inktober/jour26

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -1,13 +1,20 @@
+// Import sketch objects
+//import Pen from './pen.js';
+
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
+const horizontal = 30 * 300;
+const vertical = 20 * 300;
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
 	p5: true,
-	dimensions: [30 * 300, 20 * 300],
+	dimensions: [horizontal, vertical],
 	units: 'px',
-	//pixelsPerInch: 72,
+	bleed: 1 * 300,
+	// dimension 14 x 20 avec bleed
+	// pixelsPerInch: 72,
 
 	// Turn on a render loop
 	animate: true,
@@ -17,69 +24,73 @@ const preload = () => {
 	// You can use p5.loadImage() here, etc...
 };
 
-canvasSketch((context) => {
+canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup
 	// Like p5.js 'setup' function
+	//const Bubble = require('./Bubble');
 
-	blendMode(BLEND);
+	//blendMode(ADD);
 	colorMode(HSB, 360, 100, 100, 100);
-	background(0, 0, 10);
-	ellipseMode(CENTER);
 
-	let margin = 1200;
-	let wSpacing = 100;
-	let hSpacing = 10;
-	let xoff = 0.0006;
-	let yoff = 0.001;
-	let woff = 0.00003;
-	let wContainer = wSpacing / 5;
-	let hContainer = hSpacing;
-	let minW = wSpacing / 2;
-	let maxW = wSpacing / 2;
+	let wCenter = width / 2;
+	let hCenter = height / 2;
+	let baseHeight = height / 1.4;
+	let alpha = 0.1;
+	let angle = PI / 4;
 
-	strokeWeight(15);
-	stroke(60, 5, 95, 100);
-	noFill();
-	rect(600, 600, width - 1200, height - 1200);
+	let radius = random(800, 1000);
+	let sunPositionY = random(600 + radius, height / 3);
+	let sunPositionX = random(600 + radius, width - (600 + radius));
 
-	rectMode(CENTER);
-	paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, minW, maxW);
+	let slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 
-	// Return a renderer, which is like p5.js 'draw' function
-	return ({ p5, time, width, height }) => {
-		// Draw with p5.js things
-	};
-}, settings);
+	function branch(len, sw, alpha) {
+		stroke(60, 5, 95, alpha);
+		strokeCap(ROUND);
+		strokeWeight(sw);
+		line(0, 0, 0, -len);
+		//point(-len, 0, 0, -len);
+		translate(0, -len);
 
-function paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, minW, maxW) {
-	for (let iy = margin; iy < height - margin; iy = iy + hSpacing) {
-		for (let ix = margin; ix <= width - margin; ix = ix + wSpacing) {
-			//debugGrid(ix, iy, wSpacing, hSpacing);
-			cx = ix;
-			cy = iy;
-
-			cx2 = random(cx - maxW / 2, cx + maxW / 2);
-			cy2 = random(cy - maxW / 2, cy + maxW / 2);
-			elw = random(minW, maxW);
-
-			strokeWeight(random(5, 20));
-			noFill();
-			stroke(60, 5, 95, 10);
-			line(cx, cy, cx2, cy2);
-
-			noStroke();
-			fill(60, 5, 95, 10);
-			line(cx2, cy2, cx2 + 50, cy2 + 50);
-
-			noFill();
-			stroke(0, 0, 10);
-			//rect(cx2, cy2, elw, elw);
+		if (len > 100) {
+			push();
+			rotate(angle);
+			branch(len * 0.75, sw * 0.7, alpha * 1.5);
+			pop();
+			push();
+			rotate(0);
+			branch(len * 0.75, sw * 0.7, alpha * 1.5);
+			pop();
+			push();
+			rotate(-angle);
+			branch(len * 0.75, sw * 0.7, alpha * 1.5);
+			pop();
 		}
 	}
-}
-function debugGrid(ix, iy, wSpacing, hSpacing) {
-	strokeWeight(5);
-	stroke(0, 100, 100);
-	noFill();
-	rect(ix, iy, wSpacing, hSpacing);
-}
+
+	// Return a renderer, which is like p5.js 'draw' function
+	return ({ p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
+		// Draw with p5.js things
+		background(0, 0, 10);
+
+		// -- Frame -- //
+		strokeWeight(15);
+		stroke(60, 5, 95, 100);
+		noFill();
+		rect(600, 600, width - 1200, height - 1200);
+		// --      -- //
+		//fill(60, 5, 95, 100);
+		//ellipse(sunPositionX, sunPositionY, radius, radius);
+		angle = slider.value();
+		translate(wCenter, baseHeight);
+		branch(1000, 50, 1);
+
+		exporting = true;
+		if (!exporting && bleed > 0) {
+			stroke(0);
+			noFill();
+			strokeWeight(10);
+			rect(bleed, bleed, trimWidth, trimHeight);
+		}
+	};
+}, settings);
