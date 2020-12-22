868a702bb5eb8ec9b3bb29031384eb452881cbc8
Jonathan Barbeau <jonathan@instynctweb.com>
2020-12-22 17:17:13 -0500

    new project

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -1,11 +1,11 @@
 // Import sketch objects
-import Pen from './pen.js';
+//import Points from './points.js';
 
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
-const horizontal = 30 * 300;
-const vertical = 20 * 300;
+const horizontal = 18 * 300;
+const vertical = 18 * 300;
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
@@ -34,43 +34,6 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	background(0, 0, 10);
 	ellipseMode(CENTER);
 
-	let pen = [];
-	let xOff1 = 0;
-	let xOff2 = 0;
-	let startX1 = 0;
-	let startX2 = 0;
-
-	let elX1 = random(1000, width / 2);
-	let elY1 = random(1000, height / 2);
-	let elX2 = random(width / 2, width - 1000);
-	let elY2 = random(1000, height / 2);
-
-	for (let i = 0; i < 3; i++) {
-		pen[i] = new Pen(startX1, startX2);
-		console.log(pen[i]);
-		startX1 += 300;
-		startX2 += 200;
-	}
-
-	function makeSun() {
-		fill(0, 0, 10, 100);
-		ellipse(elX1, elY1, 1000, 1000);
-		fill(60, 5, 95, 100);
-		ellipse(elX2, elY1, 1000, 1000);
-	}
-
-	for (let i = 0; i < 6600; i++) {
-		pen[0].move(xOff1, xOff2);
-		pen[0].display();
-		xOff1 += 0.0004;
-		xOff2 += 0.0002;
-	}
-	//makeSun();
-
-	strokeWeight(15);
-	stroke(60, 5, 95, 100);
-	noFill();
-	rect(600, 600, width - 1200, height - 1200);
 	// Visualize the trim area with a yellow guide (ignored on export)
 
 	// Return a renderer, which is like p5.js 'draw' function
