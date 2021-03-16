972ef853b45071f79184edf965e87320ff8d1770
Jonathan Barbeau <jonathan@instynctweb.com>
2021-03-15 20:08:21 -0400

    prepped for print

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -28,27 +28,44 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 
 	colorMode(HSB, 360, 100, 100, 100);
-
+	background(207, 33, 94);
 	let xoff = 0;
 	let yoff = 1;
-
 	let woff = 0;
+	let x = map(noise(xoff), 0, 1, 500, width - 500);
+	let y = map(noise(yoff), 0, 1, 500, height - 500);
+	let w = map(noise(woff), 0, 1, 200, 350);
+	for (let index = 0; index < 60000; index++) {
+		x = map(noise(xoff), 0, 1, 500, width - 500);
+		y = map(noise(yoff), 0, 1, 500, height - 500);
+		xoff += 0.002;
+		yoff += 0.0021;
+		if (index >= 55000) {
+			if (w > 0) {
+				w = w - 1;
+				strokeWeight(5);
+				fill(357, 26, 96);
+				stroke(358, 46, 67);
+				ellipse(x, y, w, w);
+			} else {
+				blendMode(OVERLAY);
+				strokeWeight(10);
+				fill(357, 0, 96);
+				stroke(355, 0, 65);
+				ellipse(x, y, w, w);
+			}
+		} else {
+			w = map(noise(woff), 0, 1, 200, 350);
+			woff += 0.05;
+			strokeWeight(5);
+			fill(357, 26, 96);
+			stroke(358, 46, 67);
+			ellipse(x, y, w, w);
+		}
+	}
 
-	background(0, 0, 15);
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({ p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
-		let x = map(noise(xoff), 0, 1, 300, width - 300);
-		let y = map(noise(yoff), 0, 1, 300, height - 300);
-		let w = map(noise(woff), 0, 1, 200, 350);
-
-		xoff += 0.01;
-		yoff += 0.011;
-		woff += 0.05;
-		strokeWeight(4);
-		fill(0, 0, 15);
-		stroke(51, 31, 74);
-		ellipse(x, y, w, w);
-
 		exporting = true;
 		if (!exporting && bleed > 0) {
 			stroke(0, 100, 100);
