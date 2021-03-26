bbc47bb191027b3606984da41868610362cef0d5
Jonathan Barbeau <jonathan@instynctweb.com>
2021-03-26 18:28:49 -0400

    eye wip

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -6,13 +6,15 @@ const p5 = require('p5');
 new p5();
 const horizontal = 18 * 300;
 const vertical = 18 * 300;
+//const horizontal = 1080;
+//const vertical = 1080;
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
 	p5: true,
 	dimensions: [horizontal, vertical],
 	units: 'px',
-	bleed: 1 * 300,
+	//bleed: 1 * 300,
 	// dimension 14 x 20 avec bleed
 	// pixelsPerInch: 72,
 
@@ -27,56 +29,61 @@ const preload = () => {
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	let t = 0;
+	let strokeW = 2;
+	let alpha = 100;
+	let radius = random(width / 3, width / 3);
+	let resolution = 500;
+
+	let posX = width / 2;
+	let posY = height / 2;
+
+	let noiseIntensity = 50;
+	let noiseAmplitude = 1.05;
 
 	colorMode(HSB, 360, 100, 100, 100);
 	background(0, 50, 12, 100);
-	//blendMode(SCREEN);
-	ellipseMode(CENTER);
-	noStroke();
-	fill(26, 13, 90);
-	ellipse(random(width / 500, width / 1.3), random(height / 15, height / 8), 700);
 
-	stroke(160, 93, 35, 20);
-	strokeWeight(12);
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({ p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight }) => {
-		// Draw with p5.js things
-		var x1 = width * noise(t + 0);
-		var x2 = width * noise(t + 40);
-		var x3 = width * noise(t + 30);
-		var x4 = width * noise(t + 400);
-		var x5 = width * noise(t + 242);
-		var y1 = height * noise(t + 0);
-		var y2 = height * noise(t + 96);
-		var y3 = height * noise(t + 7);
-		var y4 = height * noise(t + 8);
-		var y5 = height * noise(t + 75);
+		if (radius <= width / 8) {
+			radius += -5;
+			resolution = resolution / 1.04;
 
-		fill(160, 93, 35, 100);
-		stroke(29, 39, 81, 100);
-		strokeWeight(5);
+			if (radius <= width / 10) {
+				strokeW = strokeW * 1.015;
+				radius += -10;
+			}
+			if (radius <= 0) {
+				radius = 0;
+				strokeW = 0;
+			}
+		} else {
+			radius += -5;
+		}
+		push();
+		translate(posX, posY);
+		strokeWeight(strokeW);
 		beginShape();
-
-		curveVertex(x1, y1);
-		curveVertex(x2, y2);
-		curveVertex(x3, y3);
-		curveVertex(x4, y4);
-		curveVertex(x5, y5);
+		for (let a = 0; a < TWO_PI; a += TWO_PI / resolution) {
+			const noiseVal = map(noise(cos(a) * noiseIntensity + 1, sin(a) * noiseIntensity + 1, time), 0, 1, noiseAmplitude, 1.0);
+			const r = radius + noiseVal;
+			const x = cos(a) * r * noiseVal;
+			const y = sin(a) * r * noiseVal;
+			if (radius <= width / 10) {
+				alpha = alpha / 1;
+				fill(285, 24, 25, alpha);
+				stroke(29, 39, 81, alpha);
+				ellipse(x, y, 1, 1);
+			} else {
+				fill(285, 24, 25, alpha);
+				stroke(29, 39, 81, alpha);
+				curveVertex(x, y);
+			}
+		}
+		console.log(resolution);
 
 		endShape(CLOSE);
-		stroke(70, 39, 81, 100);
-		strokeWeight(5);
-		//point(x1, y1);
-		//point(x2, y2);
-		//point(x3, y3);
-		//point(x4, y4);
-		t += 0.01;
-		exporting = true;
-		if (!exporting && bleed > 0) {
-			stroke(0, 100, 100);
-			noFill();
-			strokeWeight(10);
-			rect(bleed, bleed, trimWidth, trimHeight);
-		}
+		pop();
+		//posY = posY + 10;
 	};
 }, settings);
