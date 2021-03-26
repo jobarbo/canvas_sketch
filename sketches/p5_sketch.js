15e53c06c36024c53dbe66fd402b116d0a3941f9
Jonathan Barbeau <jonathan@instynctweb.com>
2021-03-26 16:32:48 -0400

    wip

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -31,35 +31,43 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	background(207, 0, 8);
 	let xoff = 0;
 	let yoff = 1;
-	let woff = 0;
+	let woff1 = 0;
+	let woff2 = 0;
 	let x = map(noise(xoff), 0, 1, 500, width - 500);
 	let y = map(noise(yoff), 0, 1, 500, height - 500);
-	let w = map(noise(woff), 0, 1, 20, 350);
-	for (let index = 0; index < 40000; index++) {
+	let w1 = map(noise(woff1), 0, 1, 20, 350);
+	let w2 = map(noise(woff2), 0, 1, 20, 350);
+	for (let index = 0; index < 2000; index++) {
 		x = map(noise(xoff), 0, 1, 500, width - 500);
 		y = map(noise(yoff), 0, 1, 500, height - 500);
-		xoff += 0.0031;
-		yoff += 0.003;
-		if (index >= 36000) {
-			if (w > 5) {
-				w = w - 1.1;
-				strokeWeight(8);
+		xoff += 0.0011;
+		yoff += 0.001;
+		if (index >= 1600) {
+			if (w1 > 5) {
+				w1 = w1 - 1.1;
+				w2 = w2 - 1.1;
+				strokeWeight(2);
 				fill(357, 0, 85);
-				stroke(358, 46, 67);
-				ellipse(x, y, w, w);
+				stroke(358, 0, 8);
+				ellipse(x, y, w1, w2);
 			} else {
-				strokeWeight(10);
-				fill(358, 0, 8, 100);
-				stroke(358, 46, 67, 100);
-				ellipse(x, y, w, w);
+				//w1 = w2;
+				w1 = 5;
+				w2 = 5;
+				strokeWeight(2);
+				fill(358, 0, 85, 100);
+				stroke(358, 0, 8, 20);
+				ellipse(x, y, w1, w2);
 			}
 		} else {
-			w = map(noise(woff), 0, 1, 20, 350);
-			woff += 0.05;
-			strokeWeight(8);
+			w1 = map(noise(woff1), 0, 1, 20, 350);
+			w2 = map(noise(woff2), 0, 1, 20, 350);
+			woff1 += 0.005;
+			woff2 += 0.001;
+			strokeWeight(2);
 			fill(357, 0, 85);
 			stroke(358, 0, 8);
-			ellipse(x, y, w, w);
+			ellipse(x, y, w1, w2);
 		}
 	}
 
