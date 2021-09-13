5003a992107a8ac678ec1678d4abf8e79090e102
Jonathan Barbeau <jonathan@instynctweb.com>
2021-09-13 11:56:29 -0400

    Added texture post-processing

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -74,8 +74,8 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		let len = random(1700, 1800);
 
 		push();
-		translate(mouseX, mouseY);
-		branch(len, 6000, 1);
+		translate(width / 2, height / 1.3);
+		branch(len, 6000, 0.25);
 		pop();
 
 		createTexture();
@@ -108,3 +108,31 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		}
 	};
 }, settings);
+
+export default class Smudge {
+	constructor(rdnX, rdnY, w1) {
+		this.xoff = 0;
+		this.yoff = 1;
+		this.woff1 = 0;
+		this.rdnX = rdnX;
+		this.rdnY = rdnY;
+		this.rdnW1 = w1;
+		this.alpha = random(15, 60);
+	}
+
+	display() {
+		for (let index = 0; index < 500; index++) {
+			this.xoff += 0.03;
+			this.yoff += 0.02;
+			this.woff1 += 0.0055;
+
+			const w1 = map(noise(this.woff1 + this.rdnW1), 0, 1, 1, 3);
+			const x = map(noise(this.xoff + this.rdnX), 0, 1, -width / 3, width * 1.5);
+			const y = map(noise(this.yoff + this.rdnY), 0, 1, -height / 3, height * 1.5);
+
+			fill(0, 0, 100, this.alpha);
+			noStroke();
+			ellipse(x, y, w1, w1);
+		}
+	}
+}
