67ba062ec0c21d588a6a02bcc35ac90547fadb89
Jonathan Barbeau <jonathan@instynctweb.com>
2022-01-05 12:18:28 -0500

    completed

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -23,7 +23,7 @@ const settings = {
 	},
 };
 
-window.preload = () => {
+const preload = () => {
 	// You can use p5.loadImage() here, etc...
 };
 
@@ -31,10 +31,39 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	noSmooth();
 	colorMode(HSB, 360, 100, 100, 100);
+	background(50, 10, 0);
+	//let colorArr = random(palettes);
+	let colorArr = random(palettes);
+	let numPoints = 100000;
+	let margin = width / 1000;
+	let bgBright = int(random(15, 35));
+	let posX = width / 2;
+	let posY = height / 2;
+	blendMode(BLEND);
+	strokeCap(SQUARE);
+	// HeadLight d'auto asphalte mouillé
+	for (let i = 0; i < numPoints; i++) {
+		let angle = random(0, TWO_PI);
+		let scalar = random(margin, width * 1);
+		let x = posX + sin(angle) * scalar;
+		let y = posY + cos(angle) * scalar;
+		let dirX = x + 1 + sin(angle) * 2;
+		let dirY = y + 1 + cos(angle) * 5;
+		let alpha = 100;
+		let sw = random(1, 50);
+		strokeWeight(sw);
+		stroke(random(colorArr));
+		line(x, y, dirX, dirY);
+		//ellipse(x, y, sw, sw);
+	}
 
+	blendMode(BLEND);
+
+	createTexture(bgBright);
 	/**
 	 * GUI Helper
 	 */
+
 	// gui.add(module_name, 'x', 0, width, 0.00001);
 	// gui.add(module_name, 'y', 0, width, 0.00001);
 
@@ -49,3 +78,53 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		}
 	};
 }, settings);
+
+function createTexture(bgBright) {
+	let texture = [];
+
+	for (let index = 0; index < 3000; index++) {
+		const rdnX = random(0, width);
+		const rdnY = random(0, height);
+		const rdnW1 = random(5, 150);
+		texture[index] = new Smudge(rdnX, rdnY, rdnW1);
+	}
+	for (let index = 0; index < texture.length; index++) {
+		for (let j = 0; j < 1000; j++) {
+			texture[index].display(bgBright);
+		}
+	}
+}
+
+export default class Smudge {
+	constructor(rdnX, rdnY, w1) {
+		this.xoff = 0;
+		this.yoff = 1;
+		this.woff1 = 0;
+		this.rdnX = rdnX;
+		this.rdnY = rdnY;
+		this.rdnW1 = w1;
+		this.mapXLow = -width / 3;
+		this.mapXHigh = width * 1.5;
+		this.mapYLow = -height / 3;
+		this.mapYHigh = height * 1.5;
+		this.alpha = int(random(1, 20));
+	}
+
+	display(bgBright) {
+		this.xoff += 0.0002;
+		this.yoff += 0.004;
+		this.woff1 += 0.55;
+
+		const w1 = map(noise(this.woff1 + this.rdnW1), 0, 1, 1, 3);
+		const x = map(noise(this.xoff + this.rdnX), 0, 1, this.mapXLow, this.mapXHigh);
+		const y = map(noise(this.yoff + this.rdnY), 0, 1, this.mapYLow, this.mapYHigh);
+
+		if (bgBright < 65) {
+			fill(0, 0, 100, this.alpha);
+		} else {
+			fill(0, 0, 0, this.alpha);
+		}
+		noStroke();
+		ellipse(x, y, w1, w1);
+	}
+}
