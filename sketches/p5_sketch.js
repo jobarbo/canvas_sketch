34919bda209534e324ccd44f1c075c27e85c3717
Jonathan Barbeau <jonathan@instynctweb.com>
2022-01-06 16:39:39 -0500

    Phillip Bell inspi

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -31,6 +31,56 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	noSmooth();
 	colorMode(HSB, 360, 100, 100, 100);
+	blendMode(MULTIPLY);
+	let pointSize = 10;
+	let pointSpacing = 15;
+	let pointAlpha = 100;
+	let margin = width / 4;
+	let xColNum = 0;
+	let yColNum = 0;
+	let elHue = 0;
+	let elW = width / 10;
+	for (let x = 0; x <= width; x += pointSpacing) {
+		for (let y = 0; y <= height; y += pointSpacing) {
+			if (xColNum % 2 == 0) {
+				pointAlpha = 0;
+			} else {
+				pointAlpha = random(50, 100);
+			}
+			if (yColNum % 2 == 0) {
+				pointAlpha = random(50, 100);
+			} else {
+				pointAlpha = 0;
+			}
+			noStroke();
+			fill(0, 100, 0, pointAlpha);
+			ellipse(x, y, pointSize);
+			yColNum++;
+		}
+		xColNum++;
+	}
+	xColNum = 0;
+	let blueXOffset = 0;
+	let redXOffset = 0;
+	for (let x = margin; x <= width - margin; x += elW) {
+		for (let y = margin; y <= height - margin; y += elW) {
+			noStroke();
+			if (xColNum % 2 == 0) {
+				elHue = 230;
+				fill(elHue, 100, 100, 100);
+				ellipse(x + blueXOffset, y, elW);
+				blueXOffset += elW / 6;
+			} else {
+				elHue = 0;
+				fill(elHue, 100, 100, 100);
+				ellipse(x + redXOffset, y, elW);
+				redXOffset -= elW / 6;
+			}
+		}
+		redXOffset = 0;
+		blueXOffset = 0;
+		xColNum++;
+	}
 
 	/**
 	 * GUI Helper
