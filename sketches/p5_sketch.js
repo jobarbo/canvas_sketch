56a969b25992a35a7a9387bebff953e5d4ceb3b8
Jonathan Barbeau <jonathan@instynctweb.com>
2022-01-08 18:17:30 -0500

    prep for today

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -31,7 +31,45 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	noSmooth();
 	colorMode(HSB, 360, 100, 100, 100);
+	background(0, 0, 100);
+	rectMode(CENTER);
+	let colorArr = random(palettes);
+	let spacing = 500;
+	let rectColor = [];
+	let rectW = width;
+	let rectB = 0;
+	let counter = 0;
+	let curveColor = random(colorArr);
+	let middleX = random(width / 2 - spacing / 2, width / 2 + spacing / 2);
+	let middleY = random(height / 2 - spacing / 2, height / 2 + spacing / 2);
+	while (rectW >= spacing) {
+		if (counter % 2 == 0) {
+			rectColor = colorArr[0];
+		} else {
+			rectColor = colorArr[4];
+		}
 
+		noStroke();
+		fill(rectColor);
+		rect(width / 2, height / 2, rectW, rectW);
+		rectW = rectW - spacing;
+
+		counter++;
+	}
+	curveColor = colorArr[4];
+	blendMode(BLEND);
+	beginShape();
+	noFill();
+	stroke(curveColor);
+	strokeWeight(spacing / 4);
+	curveVertex(width / 2 - (spacing + 25) / 2, height / 2 - (spacing + 25) / 2);
+	curveVertex(width / 2 - (spacing + 25) / 2, height / 2 - (spacing + 25) / 2);
+
+	curveVertex(middleX, middleY);
+
+	curveVertex(width / 2 + (spacing + 25) / 2, height / 2 + (spacing + 25) / 2);
+	curveVertex(width / 2 + (spacing + 25) / 2, height / 2 + (spacing + 25) / 2);
+	endShape();
 	/**
 	 * GUI Helper
 	 */
