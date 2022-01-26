5a701a9f6df292d8e29207a03b42c71bdcc53efd
Jonathan Barbeau (instynct) <51244878+Jobarbeau@users.noreply.github.com>
2022-01-25 22:18:08 -0500

    done

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -31,9 +31,7 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	noSmooth();
 	colorMode(HSB, 360, 100, 100, 100);
-	background(54, 0, 100);
-
-	let state = 1;
+	background(54, 0, 0);
 
 	let m = 256;
 	let a = 11;
@@ -62,6 +60,8 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// gui.add(module_name, 'x', 0, width, 0.00001);
 	// gui.add(module_name, 'y', 0, width, 0.00001);
 
+	createTexture();
+
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
 		exporting = true;
@@ -73,3 +73,43 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		}
 	};
 }, settings);
+
+function createTexture(bgHue, bgSat, bgBright) {
+	let texture = [];
+
+	for (let index = 0; index < 5000; index++) {
+		const rdnX = random(0, width);
+		const rdnY = random(0, height);
+		const rdnW1 = random(5, 150);
+		texture[index] = new Smudge(rdnX, rdnY, rdnW1);
+		texture[index].display(bgBright);
+	}
+}
+
+export default class Smudge {
+	constructor(rdnX, rdnY, w1) {
+		this.xoff = 0;
+		this.yoff = 1;
+		this.woff1 = 0;
+		this.rdnX = rdnX;
+		this.rdnY = rdnY;
+		this.rdnW1 = w1;
+		this.alpha = int(random(5, 15));
+	}
+
+	display(bgBright) {
+		for (let index = 0; index < 1000; index++) {
+			this.xoff += 0.003;
+			this.yoff += 0.003;
+			this.woff1 += 0.55;
+
+			const w1 = map(noise(this.woff1 + this.rdnW1), 0, 1, 1, 3);
+			const x = map(noise(this.xoff + this.rdnX), 0, 1, -1000, width + 1500);
+			const y = map(noise(this.yoff + this.rdnY), 0, 1, -1000, height + 1500);
+
+			fill(0, 0, 100, this.alpha);
+			noStroke();
+			ellipse(x, y, w1, w1);
+		}
+	}
+}
