d9438f59a42ad8c85f8992b4fad92e2cc3194f6e
Jonathan Barbeau <jonathan@instynctweb.com>
2022-01-26 23:56:06 -0500

    dne

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -31,14 +31,13 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	noSmooth();
 	colorMode(HSB, 360, 100, 100, 100);
-	background(0, 0, 100);
 
 	/**
 	 * GUI Helper
 	 */
 	// gui.add(module_name, 'x', 0, width, 0.00001);
 	// gui.add(module_name, 'y', 0, width, 0.00001);
-
+	createTexture();
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
 		exporting = true;
@@ -50,3 +49,48 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		}
 	};
 }, settings);
+function createTexture() {
+	let texture = [];
+
+	for (let index = 0; index < 3000; index++) {
+		const rdnX = random(0, width);
+		const rdnY = random(0, height);
+		const rdnW1 = random(5, 150);
+		texture[index] = new Smudge(rdnX, rdnY, rdnW1);
+	}
+	for (let index = 0; index < texture.length; index++) {
+		for (let j = 0; j < 1000; j++) {
+			texture[index].display();
+		}
+	}
+}
+
+export default class Smudge {
+	constructor(rdnX, rdnY, w1) {
+		this.xoff = 0;
+		this.yoff = 1;
+		this.woff1 = 0;
+		this.rdnX = rdnX;
+		this.rdnY = rdnY;
+		this.rdnW1 = w1;
+		this.mapXLow = -width / 3;
+		this.mapXHigh = width * 1.5;
+		this.mapYLow = -height / 3;
+		this.mapYHigh = height * 1.5;
+		this.alpha = int(random(5, 25));
+	}
+
+	display() {
+		this.xoff += 0.003;
+		this.yoff += 0.00008;
+		this.woff1 += 0.55;
+
+		const w1 = map(noise(this.woff1 + this.rdnW1), 0, 1, 1, 3);
+		const x = map(noise(this.xoff + this.rdnX), 0, 1, this.mapXLow, this.mapXHigh);
+		const y = map(noise(this.yoff + this.rdnY), 0, 1, this.mapYLow, this.mapYHigh);
+
+		fill(0, 0, 0, this.alpha);
+		noStroke();
+		ellipse(x, y, w1, w1);
+	}
+}
