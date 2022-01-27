ac48abbce54415b1205ca6444f9595d403fe935d
Jonathan Barbeau <jonathan@instynctweb.com>
2022-01-26 23:20:37 -0500

    done

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -36,62 +36,55 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 
 	let chosenPalettes = random(palettes);
 	let margin = 0;
-	let wSpacing = width * 1.5;
-	let hSpacing = height * 1.5;
+	let wSpacing = width * 2;
+	let hSpacing = height * 2;
 	let xoff = 0.6;
 	let yoff = 0.1;
 	let woff = 0.3;
-	let wContainer = wSpacing;
-	let hContainer = hSpacing;
+	let wContainer = wSpacing * 2;
+	let hContainer = hSpacing * 2;
 
 	background(chosenPalettes[0]);
 	//displayStars();
 	window.mousePressed = () => {};
-	//paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
+	for (let i = 0; i < 1000; i++) {
+		xoff = random(0, 1);
+		yoff = random(0, 1);
+		woff = random(0, 1);
+		paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, chosenPalettes);
+	}
 
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
 		// Draw with p5.js things
-		if (mouseIsPressed) {
-			xoff = random(0, 1);
-			yoff = random(0, 1);
-			woff = random(0, 1);
+		/*if (mouseIsPressed) {
+
 			paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, chosenPalettes);
-		}
+		}*/
 	};
 }, settings);
 
 function paint(margin, wSpacing, hSpacing, xoff, yoff, woff, wContainer, hContainer, chosenPalettes) {
 	let cx, cy;
+	let maxW = 10;
 	let paletteIndex = floor(random(1, 5));
 	let seamColor = color(chosenPalettes[paletteIndex]);
-	for (let iy = margin; iy < height - margin; iy = iy + hSpacing) {
-		for (let ix = margin; ix < width - margin; ix = ix + wSpacing) {
-			//debugGrid(ix,iy,wSpacing,hSpacing);
-			cx = ix + wSpacing / 2;
-			cy = iy + hSpacing / 2;
 
-			for (let s = 0; s < wSpacing * 2; s++) {
-				let x = map(noise(xoff), 0, 1, mouseX - wContainer, mouseX + wContainer);
-				let y = map(noise(yoff), 0, 1, mouseY - hContainer, mouseY + hContainer);
-				let elW = map(noise(woff), 0, 1, 0, 10);
+	for (let s = 0; s < wSpacing * 2; s++) {
+		let x = map(noise(xoff), 0, 1, width / 2 - wContainer, width / 2 + wContainer);
+		let y = map(noise(yoff), 0, 1, height / 2 - hContainer, height / 2 + hContainer);
+		let elW = map(noise(woff), 0, 1, 0, maxW);
 
-				let elHue = map(elW, 2, 8, 45, 0, true);
-				let elSat = map(elW, 2, 8, 5, 75, true);
-				let elBright = map(elW, 2, 8, 98, 10, true);
-				let elAlpha = map(elW, 2, 8, 0, 50, true);
+		let elAlpha = map(elW, maxW / 3, maxW, 0, 50, true);
 
-				noStroke();
-				//stroke(190, 53, 89,0);
-				seamColor.setAlpha(elAlpha);
-				fill(seamColor);
-				//fill(0, 75, 10, elAlpha);
-				ellipse(x, y, elW, elW);
+		noStroke();
+		// stroke(190, 53, 89, 0);
+		seamColor.setAlpha(elAlpha);
+		fill(seamColor);
+		ellipse(x, y, elW, elW);
 
-				xoff += random(0.00005, 0.0009);
-				yoff += random(0.00005, 0.0009);
-				woff += 0.1;
-			}
-		}
+		xoff += random(0.00005, 0.0005);
+		yoff += random(0.00005, 0.0005);
+		woff += 0.1;
 	}
 }
