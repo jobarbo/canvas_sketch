d3b1d31eb9c06866048958cd3ea6fc71edc0bd05
Jonathan Barbeau (instynct) <51244878+Jobarbeau@users.noreply.github.com>
2022-04-11 15:22:47 -0400

    start of something new

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -30,7 +30,11 @@ const preload = () => {
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	colorMode(HSB, 360, 100, 100, 100);
-
+	background(210, 23, 92);
+	let landMinY = height / 4;
+	let landMaxY = height / 2;
+	let landHue = 175;
+	let yoff = 0.0;
 	/**
 	 * GUI Helper
 	 */
@@ -40,6 +44,43 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
+		strokeWeight(10);
+		// We are going to draw a polygon out of the wave points
+		beginShape();
+		noFill();
+
+		let xoff = 0; // Option #1: 2D Noise
+		// let xoff = yoff; // Option #2: 1D Noise
+
+		// Iterate over horizontal pixels
+		for (let x = -100; x <= width + 100; x += 100) {
+			// Calculate a y value according to noise, map to
+
+			// Option #1: 2D Noise
+			let y = map(noise(xoff, yoff), 0, 1, landMinY, landMaxY);
+			let h = map(noise(xoff, yoff), 0, 1, 60, 180);
+
+			// Option #2: 1D Noise
+			// let y = map(noise(xoff), 0, 1, 200,300);
+			stroke(h, 55, 55, 20);
+			fill(h, 55, 65);
+			// Set the vertex
+			if (landMinY < height) {
+				curveVertex(x, y);
+				xoff += 0.05;
+				landMinY += 0.025;
+				landMaxY += 0.025;
+				//console.log(landMinY);
+			}
+
+			// Increment x dimension for noise
+		}
+		// increment y dimension for noise
+		yoff += 0.01;
+		vertex(width + 100, height + 100);
+		vertex(100, height + 100);
+		endShape(CLOSE);
+
 		exporting = true;
 		if (!exporting && bleed > 0) {
 			stroke(0, 100, 100);
