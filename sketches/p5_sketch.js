a65a256b4a18c3202da87cb0e5e6eed8e8f53bf8
Jonathan Barbeau (instynct) <51244878+Jobarbeau@users.noreply.github.com>
2022-07-15 13:59:01 -0400

    got sound working

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -1,14 +1,15 @@
 // Import sketch objects
 import Stars from "./stars.js";
-import * as dat from "dat.gui";
 const palettes = require("nice-color-palettes/1000.json");
 const canvasSketch = require("canvas-sketch");
 const p5 = require("p5");
+window.p5 = p5;
+require("p5/lib/addons/p5.sound");
 new p5();
+
+// Setup the sketch resolution
 const horizontal = 12 * 300;
 const vertical = 12 * 300;
-const mic = "";
-const gui = new dat.GUI({closed: true});
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
@@ -22,27 +23,29 @@ const settings = {
 		antialias: true,
 	},
 };
-
-const preload = () => {
-	mic = new p5.AudioIn();
-	console.log(mic);
-	// You can use p5.loadImage() here, etc...
+// Optionally preload before you load the sketch
+window.preload = () => {
+	// Preload sounds/images/etc...
 };
 
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
-	colorMode(HSB, 360, 100, 100, 100);
-	background(10, 10, 90);
+	let mic = new p5.AudioIn();
 
-	mic.start();
+	let micLevel = "";
+	window.mouseClicked = () => {
+		mic.start();
+	};
+	colorMode(HSB, 360, 100, 100, 100);
+	let bgHue = 10;
+
+	background(bgHue, 10, 0);
 
 	// let entity = new Entity(width / 2, height / 2, 150);
 	let stars = [];
-	for (let i = 0; i < 50; i++) {
-		stars[i] = new Stars(randomGaussian(width / 2, 500), randomGaussian(height / 2, 500), 50, mic);
-	}
-	for (let frame = 0; frame < 5000; frame++) {
-		for (let i = 0; i < 50; i++) {}
+	let starsNum = 500;
+	for (let i = 0; i < starsNum; i++) {
+		stars[i] = new Stars(randomGaussian(width / 2, 500), randomGaussian(height / 2, 500), 50);
 	}
 
 	/**
@@ -54,12 +57,18 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
-		//entity.display();
-		//entity.move();
-		for (let i = 0; i < 50; i++) {
-			stars[i].oscilateWeight();
-			stars[i].move();
-			stars[i].display();
+		// p5.js stuff => Like p5.js 'draw' function
+		console.log(bgHue);
+		// map micLevel to hue from 0 to 1 to 0 to 360
+		bgHue = map(micLevel, 0.03, 0.15, 0, 360);
+		bgHue = constrain(bgHue, 0, 360);
+
+		background(bgHue, 10, 0, 5);
+		micLevel = mic.getLevel();
+		for (let i = 0; i < starsNum; i++) {
+			stars[i].display(micLevel);
+			stars[i].move(micLevel);
+			stars[i].oscilateWeight(micLevel);
 		}
 		exporting = true;
 		if (!exporting && bleed > 0) {
--- sketches/stars.js
+++ sketches/stars.js
@@ -4,37 +4,39 @@ export default class Stars {
 		this.y = y;
 		this.w = w;
 		this.a = 10;
-		this.mic = mic;
+		this.h = 0;
+
 		// Module ready to be built
 	}
 
-	display() {
+	display(micLevel) {
+		this.h = map(micLevel, 0.03, 0.15, 0, 360);
+		this.h = constrain(this.h, 0, 360);
 		stroke(10, 10, 90, 100);
-		fill(0, 100, 100, this.a);
+		fill(this.h, 100, 100, this.a);
 		ellipse(this.x, this.y, this.w);
 	}
 
-	move() {
-		this.x += random(-10, 10);
-		this.y += random(-10, 10);
+	move(micLevel) {
+		this.x += random(-1, 1);
+		this.y += random(-this.w / 3, this.w / 3);
 		if (this.x >= width) {
-			this.x = width;
-		} else if (this.x <= 0) {
 			this.x = 0;
+		} else if (this.x <= 0) {
+			this.x = width;
 		}
 	}
-	// get the level of the mic input
-	getLevel() {
-		return this.mic.getLevel();
-	}
-	oscilateWeight() {
-		console.log(this.mic.getLevel());
-		this.w += random(-5, 5);
+
+	oscilateWeight(micLevel) {
+		// map micLevel from 0 to 1 to 0 to 100
+		this.w = map(micLevel, 0.03, 0.15, 0, 500);
+		this.w = constrain(this.w, 0, 500);
+		//this.w += random(-5, 5);
 		// do not let the weight go below 0
 		if (this.w <= 0) {
 			this.w = 0;
-		} else if (this.w >= 100) {
-			this.w = 100;
+		} else if (this.w >= 300) {
+			this.w = 300;
 		}
 	}
 }
