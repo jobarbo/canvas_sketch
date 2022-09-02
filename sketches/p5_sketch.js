5f2e037570861d2fd97455a025b20db839ea3009
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-02 17:51:58 -0400

    02-weekend

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -1,21 +1,21 @@
 // Import sketch objects
-import Stars from "./stars.js";
-const palettes = require("nice-color-palettes/1000.json");
-const canvasSketch = require("canvas-sketch");
-const p5 = require("p5");
+import Stars from './stars.js';
+const palettes = require('nice-color-palettes/100.json');
+const canvasSketch = require('canvas-sketch');
+const p5 = require('p5');
 window.p5 = p5;
-require("p5/lib/addons/p5.sound");
+require('p5/lib/addons/p5.sound');
 new p5();
 
 // Setup the sketch resolution
-const horizontal = 12 * 300;
-const vertical = 12 * 300;
+const horizontal = 9 * 300;
+const vertical = 16 * 300;
 
 const settings = {
 	// Pass the p5 instance, and preload function if necessary
 	p5: true,
 	dimensions: [horizontal, vertical],
-	units: "px",
+	units: 'px',
 	//duration: 30,
 	//fps: 60,
 	animate: true,
@@ -28,24 +28,86 @@ window.preload = () => {
 	// Preload sounds/images/etc...
 };
 
+// make a function that will convert an hex color value to HSL values
+const hexToHSL = (hex) => {
+	// Convert hex to RGB first
+	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	let r1 = parseInt(result[1], 16);
+	let g1 = parseInt(result[2], 16);
+	let b1 = parseInt(result[3], 16);
+	let r = r1 / 255;
+	let g = g1 / 255;
+	let b = b1 / 255;
+	let max = Math.max(r, g, b);
+	let min = Math.min(r, g, b);
+	let h = (max + min) / 2;
+	let l = (max + min) / 2;
+	let s = (max + min) / 2;
+	// If max and min are the same, the hue is undefined
+	if (max === min) {
+		h = 0;
+	} else {
+		// Calculate hue
+		if (max === r) {
+			h = (g - b) / (max - min);
+		} else if (max === g) {
+			h = 2 + (b - r) / (max - min);
+		} else if (max === b) {
+			h = 4 + (r - g) / (max - min);
+		}
+		h = h * 60;
+		if (h < 0) {
+			h += 360;
+		}
+	}
+	// Calculate lightness
+	l = ((max + min) / 2) * 100;
+	// Calculate saturation
+	s = 0;
+	if (l > 0 && l < 50) {
+		s = ((max - min) / (max + min)) * 100;
+	} else if (l >= 50 && l < 100) {
+		s = ((max - min) / (2 - max - min)) * 100;
+	}
+	h = int(h);
+	s = int(s);
+	l = int(l);
+	return [h, s, l];
+};
+
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
+
+	colorMode(HSL, 360, 100, 100, 100);
+	let palette = ['#000035', '#000026', '#001d3d', '#ffeb99', '#ffe270'];
+
+	// chose a random palette for the background color
+	let bgColor = palette[Math.floor(Math.random() * palette.length)];
+	bgColor = hexToHSL(bgColor);
+	console.log(bgColor);
+
 	let mic = new p5.AudioIn();
 
-	let micLevel = "";
+	let micLevel = '';
 	window.mouseClicked = () => {
 		mic.start();
+		getAudioContext().resume();
 	};
-	colorMode(HSB, 360, 100, 100, 100);
-	let bgHue = 10;
-
-	background(bgHue, 10, 0);
 
 	// let entity = new Entity(width / 2, height / 2, 150);
 	let stars = [];
 	let starsNum = 500;
 	for (let i = 0; i < starsNum; i++) {
-		stars[i] = new Stars(randomGaussian(width / 2, 500), randomGaussian(height / 2, 500), 50);
+		// chose a random palette for the stars color that is not the background color
+		let starsColor = palette[Math.floor(Math.random() * palette.length)];
+		starsColor = hexToHSL(starsColor);
+		if (starsColor[0] === bgColor[0] && starsColor[1] === bgColor[1] && starsColor[2] === bgColor[2]) {
+			while (starsColor[0] === bgColor[0] && starsColor[1] === bgColor[1] && starsColor[2] === bgColor[2]) {
+				starsColor = palette[Math.floor(Math.random() * palette.length)];
+				starsColor = hexToHSL(starsColor);
+			}
+		}
+		stars[i] = new Stars(randomGaussian(width / 2, 300), randomGaussian(height / 2, 500), random(1, 100), starsColor, bgColor);
 	}
 
 	/**
@@ -58,18 +120,17 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
 		// p5.js stuff => Like p5.js 'draw' function
-		console.log(bgHue);
 		// map micLevel to hue from 0 to 1 to 0 to 360
-		bgHue = map(micLevel, 0.03, 0.15, 0, 360);
-		bgHue = constrain(bgHue, 0, 360);
+		background(bgColor[0], bgColor[1], bgColor[2], 100);
 
-		background(bgHue, 10, 0, 5);
 		micLevel = mic.getLevel();
 		for (let i = 0; i < starsNum; i++) {
 			stars[i].display(micLevel);
 			stars[i].move(micLevel);
 			stars[i].oscilateWeight(micLevel);
 		}
+		fill(bgColor[0], bgColor[1], bgColor[2], 11);
+		rect(0, 0, width, height);
 		exporting = true;
 		if (!exporting && bleed > 0) {
 			stroke(0, 100, 100);
--- sketches/stars.js
+++ sketches/stars.js
@@ -1,42 +1,56 @@
 export default class Stars {
-	constructor(x, y, w, mic) {
+	constructor(x, y, w, starsColor, bgColor) {
 		this.x = x;
 		this.y = y;
-		this.w = w;
-		this.a = 10;
-		this.h = 0;
+		this.initW = w;
+		this.maxW = w * random(2, 4);
+		this.minW = w * random(0.5, 0.8);
+		this.w = this.minW;
+		this.a = random(0.01, 5);
+		this.minA = this.a * random(0.5, 1);
+		this.h = starsColor[0];
+		this.s = starsColor[1];
+		this.l = starsColor[2];
+		this.sh = bgColor[0];
+		this.ss = bgColor[1];
+		this.sl = bgColor[2];
+		this.offset = random(0.075, 0.3);
+		this.minOffset = random(0.0, 0.03);
+		this.moveOffset = 1;
 
 		// Module ready to be built
 	}
 
 	display(micLevel) {
-		this.h = map(micLevel, 0.03, 0.15, 0, 360);
-		this.h = constrain(this.h, 0, 360);
-		stroke(10, 10, 90, 100);
-		fill(this.h, 100, 100, this.a);
+		strokeWeight(2);
+		stroke(this.sh, this.ss, this.sl, this.a);
+		fill(this.h, this.s, this.l, this.a);
 		ellipse(this.x, this.y, this.w);
 	}
 
 	move(micLevel) {
-		this.x += random(-1, 1);
-		this.y += random(-this.w / 3, this.w / 3);
+		this.moveOffset = map(micLevel, 0.0, 0.1, this.moveOffset - 1, this.moveOffset + 1);
+		this.moveOffset = constrain(this.moveOffset, 0, 10);
+		this.y += random(-this.moveOffset, this.moveOffset);
+		this.x += random(-this.moveOffset / 20, this.moveOffset / 20);
+		this.a += random(-this.moveOffset, this.moveOffset);
+		this.a = constrain(this.a, 0, 100);
+
 		if (this.x >= width) {
 			this.x = 0;
 		} else if (this.x <= 0) {
 			this.x = width;
 		}
+		if (this.y >= height) {
+			this.y = 0;
+		} else if (this.y <= 0) {
+			this.y = height;
+		}
 	}
 
 	oscilateWeight(micLevel) {
 		// map micLevel from 0 to 1 to 0 to 100
-		this.w = map(micLevel, 0.03, 0.15, 0, 500);
-		this.w = constrain(this.w, 0, 500);
-		//this.w += random(-5, 5);
-		// do not let the weight go below 0
-		if (this.w <= 0) {
-			this.w = 0;
-		} else if (this.w >= 300) {
-			this.w = 300;
-		}
+		this.w = map(micLevel, this.minOffset, this.offset, this.minW, this.maxW);
+		this.w = constrain(this.w, this.minW, this.maxW);
 	}
 }
