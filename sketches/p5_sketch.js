3959ecd65ad9175277b063115592d992cbffa269
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-20 17:10:18 -0400

    removed cool palette + added bleed

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -2,13 +2,12 @@
 import Entity from './entity.js';
 import * as dat from 'dat.gui';
 import HexToHsb from './utils/hexToHsb.js';
+const ColorScheme = require('color-scheme');
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
 const horizontal = 12 * 300;
 const vertical = 12 * 300;
-const ColorScheme = require('color-scheme');
-
 const gui = new dat.GUI({closed: true});
 
 const settings = {
@@ -16,8 +15,9 @@ const settings = {
 	p5: true,
 	dimensions: [horizontal, vertical],
 	units: 'px',
+	bleed: 100,
 	//duration: 30,
-	//fps: 60,
+	fps: 60,
 	animate: true,
 	attributes: {
 		antialias: true,
@@ -46,6 +46,13 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		pale
 	 */
 
+	/**
+	 * GUI Helper
+	 */
+
+	// gui.add(module_name, 'x', 0, width, 0.00001);
+	// gui.add(module_name, 'y', 0, width, 0.00001);
+
 	colorMode(HSB, 360, 100, 100, 100);
 	rectMode(CENTER);
 	const s = new ColorScheme();
@@ -55,32 +62,13 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	const convertedColor = new HexToHsb(colors);
 	const palette = convertedColor.hsb;
 
-	for (let i = 0; i < 10; i++) {
-		let chosenColor = palette[Math.floor(Math.random() * palette.length)];
-		let chosenStrokeColor = palette[Math.floor(Math.random() * palette.length)];
-		strokeWeight(15);
-
-		if (i % 3 === 0) {
-			noFill();
-			stroke(chosenStrokeColor[0], chosenStrokeColor[1], chosenStrokeColor[2]);
-		} else {
-			fill(chosenColor[0], chosenColor[1], chosenColor[2]);
-			noStroke();
-		}
-		rect(random(100, width - 100), random((100, height - 100)), random(100, 1000), random(100, 500));
-	}
-
-	/**
-	 * GUI Helper
-	 */
-
-	// gui.add(module_name, 'x', 0, width, 0.00001);
-	// gui.add(module_name, 'y', 0, width, 0.00001);
-
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
-		exporting = true;
+		// p5.js draw loop
+
+		exporting = false;
 		if (!exporting && bleed > 0) {
+			rectMode(CORNER);
 			stroke(0, 100, 100);
 			noFill();
 			strokeWeight(10);
