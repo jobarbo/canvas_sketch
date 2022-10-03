594ac722e676b1e27aa29e1e7ca6fd798700a50a
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-10-02 20:44:46 -0400

    changed movement and size

--- sketches/ball.js
+++ sketches/ball.js
@@ -4,32 +4,40 @@ export default class Ball {
 		this.x = x;
 		this.y = y;
 		if (radiusIndex === 0) {
-			this.radius = width / 10;
+			this.radius = 150;
 		} else {
 			this.radius = random(10, 120);
 		}
-		this.speed = random(-2, 2);
+		this.speed = random(-4, 4);
 		this.direction = random(-TWO_PI, TWO_PI);
 		this.angle = 0.0;
-		this.scalar = 1;
-		this.strokeColor = strokeColor;
-		this.strokeColor.setAlpha(10);
-		this.ballColor = ballColor;
+		this.scalar = 0;
+		this.strokeColor = ballColor;
+		this.strokeColor.setAlpha(100);
+		this.ballColor = strokeColor;
 		this.ballColor.setAlpha(100);
 	}
 
 	update() {
 		let ang1 = radians(this.angle);
-		this.x += this.speed * sin(ang1);
-		this.y += this.speed * cos(ang1);
+
 		//this.y += this.speed + this.scalar * sin(ang1);
+
+		if (this.radius <= 0) {
+			this.radius = 0;
+		} else {
+			this.radius += random(-1.02, 1);
+			this.x += this.speed * cos(ang1);
+			this.y += this.speed * sin(ang1);
+		}
 		this.angle += random(-10, 10);
 		this.scalar += random(-0.2, 0.2);
 	}
 
 	display() {
-		strokeWeight(3);
+		strokeWeight(1);
 		stroke(this.strokeColor);
+		fill(this.ballColor);
 		ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
 	}
 }
--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -7,8 +7,8 @@ const p5 = require('p5');
 new p5();
 
 // Setup the canvas
-const horizontal = 9 * 300;
-const vertical = 16 * 300;
+const horizontal = 10 * 300;
+const vertical = 10 * 300;
 
 const gui = new dat.GUI({closed: false});
 
@@ -33,17 +33,19 @@ window.preload = () => {
 canvasSketch((context, bleed, trimWidth, trimHeight) => {
 	// Sketch setup => Like p5.js 'setup' function
 	colorMode(HSB, 360, 100, 100, 100);
+
 	let chosenPalette = palettes[Math.floor(Math.random() * palettes.length)];
 	let backgroundColor = color(chosenPalette[Math.floor(Math.random() * chosenPalette.length)]);
+	let ballColor = complementaryColor(backgroundColor);
 
-	// choose the color of the balls from the same palette as backgroundColor but not the same color as backgroundColor
-	let ballColor = color(chosenPalette[Math.floor(Math.random() * chosenPalette.length)]);
-	console.log(ballColor.toString());
-	console.log(backgroundColor.toString());
+	/* 	// choose the color of the balls from the same palette as backgroundColor but not the same color as backgroundColor
+	let ballColor = color(chosenPalette[Math.floor(Math.random() * chosenPalette.length)]); */
+
 	while (ballColor.toString() === backgroundColor.toString()) {
 		ballColor = color(chosenPalette[Math.floor(Math.random() * chosenPalette.length)]);
 	}
-
+	console.log(ballColor.toString());
+	console.log(backgroundColor.toString());
 	background(backgroundColor);
 
 	/**
@@ -55,22 +57,27 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 
 	// create a new object with the properties you want to control
 	let balls = [];
-	for (let i = 0; i < 2; i++) {
+	for (let i = 0; i < 200; i++) {
 		balls[i] = new Ball(width / 2, height / 2, int(random(50)), backgroundColor, ballColor);
 	}
 
-	for (let i = 0; i < balls.length; i++) {
+	/* 	for (let i = 0; i < balls.length; i++) {
 		// run a loop while the balls are still on the canvas
-		while (balls[i].x > -width / 10 && balls[i].x < width + width / 10 && balls[i].y > -width / 10 && balls[i].y < height + width / 10) {
+		while (balls[i].x > -width / 10 && balls[i].x < width + width / 10 && balls[i].y > -height / 10 && balls[i].y < height + height / 10) {
 			balls[i].update();
 			balls[i].display();
 		}
-	}
+	} */
 
 	// Return a renderer, which is like p5.js 'draw' function
 	return ({p5, time, width, height, context, exporting, bleed, trimWidth, trimHeight}) => {
-		exporting = true;
+		for (let i = 0; i < balls.length; i++) {
+			// run a loop while the balls are still on the canvas
 
+			balls[i].update();
+			balls[i].display();
+		}
+		exporting = true;
 		if (!exporting && bleed > 0) {
 			stroke(0, 100, 100);
 			noFill();
@@ -79,3 +86,14 @@ canvasSketch((context, bleed, trimWidth, trimHeight) => {
 		}
 	};
 }, settings);
+
+// make a function to chose a random color that is somewhat complementary to the background color
+function complementaryColor(backgroundColor) {
+	let h = hue(backgroundColor);
+	let s = saturation(backgroundColor);
+	let b = brightness(backgroundColor);
+	let a = alpha(backgroundColor);
+	let complementaryHue = (h + 180) % 360;
+	let complementaryColor = color(complementaryHue, s, b, a);
+	return complementaryColor;
+}
