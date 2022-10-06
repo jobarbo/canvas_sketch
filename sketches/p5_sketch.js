886531a456b04af6ef16e4c0a2b8208053342fa7
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-10-06 16:52:25 -0400

    added iterator

--- sketches/p5_sketch.js
+++ sketches/p5_sketch.js
@@ -3,101 +3,101 @@ const p5 = require('p5');
 new p5();
 
 const settings = {
-  // Pass the p5 instance, and preload function if necessary
-  p5: true,
-  dimensions: [ 12*300, 12*300 ],
-  units: 'px',
-  //pixelsPerInch: 72,
+	// Pass the p5 instance, and preload function if necessary
+	p5: true,
+	dimensions: [12 * 300, 12 * 300],
+	units: 'px',
+	//pixelsPerInch: 72,
 
-  // Turn on a render loop
-  animate: true
+	// Turn on a render loop
+	animate: true,
 };
 
 const preload = () => {
-  // You can use p5.loadImage() here, etc...
-
+	// You can use p5.loadImage() here, etc...
 };
 
 canvasSketch((context) => {
-  // Sketch setup
-  // Like p5.js 'setup' function
+	// Sketch setup
+	// Like p5.js 'setup' function
 
-  blendMode(BLEND);
-  colorMode(HSB, 360, 100, 100, 100);
-  background(45, 5, 98);
+	blendMode(BLEND);
+	colorMode(HSB, 360, 100, 100, 100);
+	background(45, 5, 98);
+	let xoffIteratorArr = [0.00001, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1];
+	let yoffIteratorArr = [0.00001, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1];
 
-  let margin = 0;
-  let wSpacing = width;
-  let hSpacing = height;
-  let xoff = 0.6;
-  let yoff = 0.1;
-  let woff = 0.3;
-  let wContainer = wSpacing/1.5;
-  let hContainer = hSpacing/1.5;
+	let margin = 0;
+	let wSpacing = width;
+	let hSpacing = height;
+	let xoff = 0.6;
+	let yoff = 0.1;
 
-  //displayStars();
-  window.mousePressed = () => {
+	let woff = 0.3;
+	let xoffIterator = random(xoffIteratorArr);
+	let yoffIterator = random(yoffIteratorArr);
+	console.log(`xoffIterator: ${xoffIterator}`);
+	console.log(`yoffIterator: ${yoffIterator}`);
+	let wContainer = wSpacing / 1.5;
+	let hContainer = hSpacing / 1.5;
 
-  }
-  //paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
+	//displayStars();
 
-  // Return a renderer, which is like p5.js 'draw' function
-  return ({ p5, time, width, height }) => {
-    // Draw with p5.js things
-    if (mouseIsPressed){
-      xoff = random(0,1);
-      yoff = random(0,1);
-      woff = random(0,1);
-      paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
-    }
+	let initX = width / 2;
+	let initY = height / 2;
+	for (let i = 0; i < 50; i++) {
+		xoff = random(1);
+		yoff = random(1);
+		woff = random(1);
 
-  };
+		paint(margin, wSpacing, hSpacing, xoff, yoff, woff, xoffIterator, yoffIterator, wContainer, hContainer, initX, initY);
+	}
+	//paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer);
+
+	// Return a renderer, which is like p5.js 'draw' function
+	return ({p5, time, width, height}) => {
+		// Draw with p5.js things
+	};
 }, settings);
 
-function paint(margin,wSpacing,hSpacing,xoff,yoff,woff,wContainer,hContainer){
-  for(let iy = margin; iy < (height-margin); iy = iy + hSpacing){
+function paint(margin, wSpacing, hSpacing, xoff, yoff, woff, xoffIterator, yoffIterator, wContainer, hContainer, initX, initY) {
+	for (let s = 0; s < wSpacing; s += 1) {
+		let xn = map(noise(xoff), 0, 1, -width / 2, width / 2, true);
+		let yn = map(noise(yoff), 0, 1, -height / 2, height / 2, true);
+		let x = width / 2 + xn;
+		let y = height / 2 + yn;
 
-    for(let ix = margin; ix < (width-margin); ix = ix + wSpacing){
-      //debugGrid(ix,iy,wSpacing,hSpacing);
-      cx = ix+(wSpacing/2);
-      cy = iy+(hSpacing/2);
+		let elW = map(noise(woff), 0, 1, 0, 10);
 
-      for(let s = 0; s < wSpacing; s++){
+		let elHue = map(elW, 2, 8, 45, 0, true);
+		let elSat = map(elW, 2, 8, 5, 75, true);
+		let elBright = map(elW, 2, 8, 98, 10, true);
+		let elAlpha = map(elW, 0, 10, 0, 100, true);
 
-        let x = map(noise(xoff),0,1,mouseX-wContainer,mouseX+wContainer);
-        let y = map(noise(yoff),0,1,mouseY-hContainer,mouseY+hContainer);
-        let elW = map(noise(woff),0,1,0,10);
+		noStroke();
+		//stroke(190, 53, 89,0);
+		fill(0, 75, 10, elAlpha);
+		ellipse(x, y, elW, elW);
 
-        let elHue = map(elW,2,8,45,0,true);
-        let elSat = map(elW,2,8,5,75,true);
-        let elBright = map(elW,2,8,98,10,true);
-        let elAlpha = map(elW,2,8,0,100,true);
+		xoff += xoffIterator;
+		yoff += yoffIterator;
 
-        noStroke();
-        //stroke(190, 53, 89,0);
-        fill(0, 75, 10,elAlpha);
-        ellipse(x,y,elW,elW);
-
-        xoff += 0.001;
-        yoff += 0.001;
-        woff += 0.1;
-      }
-    }
-  }
+		woff += 0.1;
+	}
 }
 
-function displayStars(){
-  for(let i=0; i<2000;i++){
-    let starAlpha = random(1,30);
-    stroke(48,56,83,starAlpha);
-    strokeWeight(random(5,10));
-    point(random(width),random(height))
-  }
+function displayStars() {
+	for (let i = 0; i < 2000; i++) {
+		let starAlpha = random(1, 30);
+		stroke(48, 56, 83, starAlpha);
+		strokeWeight(random(5, 10));
+		point(random(width), random(height));
+	}
 }
 
-function debugGrid(ix,iy,wSpacing,hSpacing){
-  strokeWeight(5)
-  stroke(0,100,100)
-  noFill();
-  rect(ix,iy,wSpacing,hSpacing)
+function debugGrid(ix, iy, wSpacing, hSpacing) {
+	strokeWeight(5);
+	stroke(0, 100, 100);
+	noFill();
+	rect(ix, iy, wSpacing, hSpacing);
 }
